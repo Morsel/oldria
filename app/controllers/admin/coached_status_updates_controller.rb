38991cd78027ca2a6a19c8745242b787475bc9ea
Joshua Davey <josh@neotericdesign.com>
2009-09-08 11:04:39 -0500

    Adding 'Save and Create Another' option on a form

--- app/controllers/admin/coached_status_updates_controller.rb
+++ app/controllers/admin/coached_status_updates_controller.rb
@@ -14,8 +14,12 @@ class Admin::CoachedStatusUpdatesController < Admin::AdminController
   def create
     @coached_status_update = CoachedStatusUpdate.new(params[:coached_status_update])
     if @coached_status_update.save
-      flash[:notice] = "Successfully created Coached Status Update."
-      redirect_to [:admin, @coached_status_update]
+      flash[:notice] = "Successfully created Coached Status Update '#{@coached_status_update.message}'"
+      if params[:save_and_new]
+        redirect_to new_admin_coached_status_update_path
+      else
+        redirect_to admin_coached_status_updates_path
+      end
     else
       render :new
     end
@@ -29,7 +33,11 @@ class Admin::CoachedStatusUpdatesController < Admin::AdminController
     @coached_status_update = CoachedStatusUpdate.find(params[:id])
     if @coached_status_update.update_attributes(params[:coached_status_update])
       flash[:notice] = "Successfully updated coached status update."
-      redirect_to [:admin, @coached_status_update]
+      if params[:save_and_new]
+        redirect_to new_admin_coached_status_update_path
+      else
+        redirect_to admin_coached_status_updates_path
+      end
     else
       render :edit
     end
