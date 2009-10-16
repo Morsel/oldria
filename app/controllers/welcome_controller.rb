0807dfca3ed8e2e039569f9badc0a1563ade29e4
Joshua Davey <josh@neotericdesign.com>
2009-10-16 17:01:19 -0500

    Sort order

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,14 +3,14 @@ class WelcomeController < ApplicationController
     if current_user
       @user = current_user
       if @user.has_role? :media
-        @media_requests = @user.media_requests.all(:include => :conversations_with_comments)
+        @media_requests = @user.media_requests.all(:include => :conversations_with_comments, :order => "created_at DESC")
         render :mediahome
       else
         @direct_messages = @user.direct_messages.all_not_from_admin(:include => :sender)
         @sent_messages = @user.sent_direct_messages.all(:include => :receiver, :limit => 3)
         @admin_direct_messages = @user.direct_messages.all_from_admin
         @friend_activity = Status.friends_of_user(@user)
-        @media_request_conversations = @user.media_request_conversations.all(:include => :media_request, :limit => 5)
+        @media_request_conversations = @user.media_request_conversations.all(:include => :media_request, :limit => 5, :order => "created_at DESC")
         render :dashboard
       end
     else
