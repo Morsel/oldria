f82b7a687cbb85c766d6a34b264a4de1b0e0744f
Joshua Davey <josh@neotericdesign.com>
2009-10-26 15:58:54 -0500

    More maintainable seeds using yml files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
-# Set up Regions
+@seedling_path = File.expand_path(File.join(File.dirname(__FILE__), 'seedlings'))
 
+# == Set up Regions ==
 regions = {"Great Lakes" => "IL, IN, MI, OH", "Mid-Atlantic" => "D.C., DE, MD, NJ, PA, VA", "Midwest" => "IA, KS, MN, MO, NE, ND, SD, WI", "New York City" => "Five Boroughs", "Northeast" => "CT, MA, ME, NH, NY State, RI, VT", "Northwest" => "AK, ID, MT, OR, WA, WY", "Pacific" => "CA, HI", "South" => "AL, AR, FL, LA, MS", "Southeast" => "GA, KY, NC, SC, TN, WV", "Southwest" =>  "AZ, CO, NM, NV, OK, TX, UT"}
 
 regions.each_pair do |region_name, desc|
@@ -9,20 +10,27 @@ end
 
 puts "There are now #{JamesBeardRegion.count} regions"
 
-# Set up Media Request Types
+# == Set up Media Request Types ==
 
-request_types = {
-  'information' => ["Information Request", ""],
-  'photo'       => ["Photo Request", "Photo needed, Photo requirements, Other important notes"],
-  'interview'   => ["Interview Scheduling", "By phone or in person?, Convenient date and time 1, Convenient date and time 2, Convenient date and time 3, Interview subject, Are there any requirements for the interview?"], 
-  'recipe'      => ["Recipe Request", "Dish or Drink?"],
-  'photoshoot'  => ["Photo Shoot/On-Site Video Shoot Scheduling", "For which outlet is this shoot?, Requested Date and Time, Alternate Date and Time, Primary subject, Appearance requirements"]
-}
+request_types = YAML.load_file(@seedling_path + '/media_request_types.yml')
 
 request_types.each_pair do |shortname, name_and_fields|
-  name, fields = name_and_fields
+  puts name_and_fields
+  name = name_and_fields['name']
+  fields = name_and_fields['fields']
+  fields = fields.join(", ") if fields
   rt = MediaRequestType.find_by_shortname(shortname)
   rt ||= MediaRequestType.create!(:shortname => shortname, :name => name, :fields => fields)
 end
 
 puts "There are now #{MediaRequestType.count} media request types"
+
+
+# == Set up Cuisines ==
+
+cuisines = YAML.load_file(@seedling_path + '/cuisines.yml')['cuisines']
+
+
+# == Set up Metropolitan Regions ==
+
+metroregions = YAML.load_file(@seedling_path + '/metroregions.yml')['metroregions']
