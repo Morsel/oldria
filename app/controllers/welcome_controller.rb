d5a895bf29ef72e5b317b0f28a8f4eaa1a807610
Joshua Davey <josh@neotericdesign.com>
2009-11-24 18:05:08 -0600

    Fixing repeated restaurant name bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
     if current_user
       @user = current_user
       if @user.has_role? :media
-        @media_requests = @user.media_requests.for_dashboard.all(:include => [:conversations_with_comments, :media_request_type, :recipients]).group_by(&:media_request_type)
+        @media_requests_by_type = @user.media_requests.for_dashboard.all(:include => [:conversations_with_comments, :media_request_type, :recipients]).group_by(&:media_request_type)
         render :mediahome
       else
         @direct_messages = @user.direct_messages.all_not_from_admin(:include => :sender)
