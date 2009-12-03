370990bddb005084b4ac73fedca6eceb28516b1b
Joshua Davey <josh@neotericdesign.com>
2009-12-03 12:41:31 -0600

    Small admin nil bug

--- app/helpers/admin/media_requests_helper.rb
+++ app/helpers/admin/media_requests_helper.rb
@@ -2,7 +2,7 @@ module Admin::MediaRequestsHelper
   def list_and_link_replies(media_request)
     return "none" if media_request.conversations_with_comments.size == 0
     media_request.conversations_with_comments.map do |conversation|
-      link_to conversation.recipient.name, media_request_conversation_path(conversation)
+      link_to conversation.recipient.try(:name), media_request_conversation_path(conversation)
     end.join(", ")
   end
 end
