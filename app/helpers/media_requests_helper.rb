b8ba2a5a31b0de125afa74ad1609de66f43f49de
Joshua Davey <josh@neotericdesign.com>
2009-12-22 17:15:46 -0600

    Always clear advanced search check boxes

--- app/helpers/media_requests_helper.rb
+++ app/helpers/media_requests_helper.rb
@@ -38,11 +38,9 @@ module MediaRequestsHelper
   def multiple_checkbox_search(collection, search_method)
     content_tag :ul, :class => 'multiple_checkbox' do
       collection.inject("") do |result, obj|
-        checked = params[:search] && params[:search][search_method] && params[:search][search_method].include?(obj.id.to_s)
-        result += "<li><label>"
-        result += check_box_tag("search[#{search_method}][]", obj.id, checked)
-        result += obj.name
-        result += "</label></li>"
+        result + "<li><label>" +
+        check_box_tag("search[#{search_method}][]", obj.id, false) +
+        obj.name + "</label></li>"
       end
     end
   end
