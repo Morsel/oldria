d255888c5f5b95f7343d694cf35e355012f315b8
Joshua Davey <josh@neotericdesign.com>
2009-12-30 16:28:19 -0600

    Fixing up the HTML formatter for CKEditor

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -15,3 +15,29 @@ config.toolbar = 'Basic';
 config.resize_maxWidth = 933;
 config.resize_minWidth = 639;
 };
+
+// HTML writer properties
+CKEDITOR.on( 'instanceReady', function(ev){
+	var htmlWriter = ev.editor.dataProcessor.writer;
+	var formattingDefaults = {
+		indent : false,
+		breakBeforeOpen : true,
+		breakAfterOpen : false,
+		breakBeforeClose : false,
+		breakAfterClose : true
+	};
+	htmlWriter.indentationChars = "  ";
+	htmlWriter.lineBreakChars = "\n\n";
+	htmlWriter.setRules('p', formattingDefaults);
+	htmlWriter.setRules('h2', formattingDefaults);
+	htmlWriter.setRules('h3', formattingDefaults);
+	htmlWriter.setRules('h4', formattingDefaults);
+	htmlWriter.setRules('div', {
+		indent : true,
+		breakBeforeOpen : true,
+		breakAfterOpen : true,
+		breakBeforeClose : true,
+		breakAfterClose : true
+	});
+});
+
