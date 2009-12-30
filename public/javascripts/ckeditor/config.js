eab190b15fa774e887a201a3d5ae6585d8ddc7eb
Joshua Davey <josh@neotericdesign.com>
2009-12-30 16:02:32 -0600

    CKEditor: Force paste as plain text

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -1,16 +1,17 @@
-﻿CKEDITOR.editorConfig = function( config ) {
-	config.uiColor = '#E2E2E2';
-
-	config.toolbar_Basic = [
-		['Format','-','Bold', 'Italic', '-', 
-		'NumberedList', 'BulletedList', '-', 
-		'Link', 'Unlink', '-',
-		'Source']
+CKEDITOR.editorConfig = function(config) {
+config.uiColor = '#E2E2E2';
+config.toolbar_Basic = [
+		['Format'],
+		['Undo', 'Redo'],
+		['Bold', 'Italic', '-', 'NumberedList', 'BulletedList'],
+		['Find','RemoveFormat'],
+		['Link', 'Unlink', '-', 'Source']
 	];
 
-	config.format_tags = 'p;h2;h3;h4;h5;pre' ;
+config.format_tags = 'p;h2;h3;h4;h5;pre';
 
-	config.toolbar = 'Basic';
-	config.resize_maxWidth = 933;
-	config.resize_minWidth = 639;
+config.forcePasteAsPlainText = true;
+config.toolbar = 'Basic';
+config.resize_maxWidth = 933;
+config.resize_minWidth = 639;
 };
