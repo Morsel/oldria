a1591fb70bb88a3a55140ac563c0905a871d150d
Joshua Davey <josh@neotericdesign.com>
2010-01-06 12:37:24 -0600

    Limit friend activity to 10 items

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,7 @@ class WelcomeController < ApplicationController
         @direct_messages = @user.direct_messages.all_not_from_admin(:include => :sender)
         @sent_messages = @user.sent_direct_messages.all(:include => :receiver, :limit => 3)
         @admin_direct_messages = @user.direct_messages.all_from_admin
-        @friend_activity = Status.friends_of_user(@user)
+        @friend_activity = Status.friends_of_user(@user).all(:limit => 10)
         @media_request_conversations = @user.media_request_conversations.all(:include => :media_request, :limit => 5, :order => "media_requests.created_at DESC", :conditions => {:media_requests => {:status => 'approved'}})
         render :dashboard
       end
