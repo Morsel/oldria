e6fceaff5d4c9bbbb50d23e6d7dd43df14662213
Joshua Davey <josh@neotericdesign.com>
2010-01-12 17:55:29 -0600

    Rescue OAuth error that Twitter randomly throws

--- app/controllers/twitter_authorizations_controller.rb
+++ app/controllers/twitter_authorizations_controller.rb
@@ -10,27 +10,30 @@ class TwitterAuthorizationsController < ApplicationController
 
     redirect_to request_token.authorize_url
   end
-  
+
   def show
-    twitter_oauth = current_user.twitter_oauth
-    access_token = twitter_oauth.authorize(
-      session['rtoken'], 
-      session['rsecret'], 
-      :oauth_verifier => params[:oauth_verifier]
-    )
-    
-    if current_user.update_attributes({:atoken => access_token.token, :asecret => access_token.secret})
-      clear_request_tokens
-      flash[:notice] = "Cool. Your Twitter account was successfully linked to SpoonFeed."
-      redirect_to root_path
-    else
+    begin
+      twitter_oauth = current_user.twitter_oauth
+      access_token = twitter_oauth.authorize(
+        session['rtoken'],
+        session['rsecret'],
+        :oauth_verifier => params[:oauth_verifier]
+      )
+
+      if current_user.update_attributes({:atoken => access_token.token, :asecret => access_token.secret})
+        clear_request_tokens
+        flash[:notice] = "Cool. Your Twitter account was successfully linked to SpoonFeed."
+      end
+
+    rescue OAuth::Unauthorized
       flash[:error] = "Hmm, something went wrong. We'll sent our worker bees in to find out why."
-      redirect_to root_path
     end
+
+    redirect_to root_path
   end
-  
+
   private
-  
+
   def clear_request_tokens
     session['rtoken'] = nil
     session['rsecret'] = nil
