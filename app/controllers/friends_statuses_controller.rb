0b22afbd9413e3d98313abc2a37b00e28363ef2d
Joshua Davey <josh@neotericdesign.com>
2010-01-19 15:32:24 -0600

    Story 2204850 - Add avatar to folks you follow

--- app/controllers/friends_statuses_controller.rb
+++ app/controllers/friends_statuses_controller.rb
@@ -2,6 +2,10 @@ class FriendsStatusesController < ApplicationController
   before_filter :require_user
 
   def show
-    @statuses = Status.friends_of_user(current_user).paginate(:page => params[:page], :per_page => 20)
+    @statuses = Status.friends_of_user(current_user).paginate(
+      :page => params[:page],
+      :per_page => 20,
+      :include => :user
+    )
   end
 end
