355ff126f6620f3b44715ca0bfa2b4e954c69031
Joshua Davey <josh@neotericdesign.com>
2010-02-03 17:02:54 -0600

    fixing migration

--- db/migrate/20100203174304_create_feed_categories.rb
+++ db/migrate/20100203174304_create_feed_categories.rb
@@ -11,11 +11,9 @@ class CreateFeedCategories < ActiveRecord::Migration
 
     add_index :feed_categories, :id, :unique => true
     add_index :feeds, :feed_category_id
-    add_index :feeds, :id, :unique => true
   end
 
   def self.down
-    remove_index :feeds, :id
     remove_index :feeds, :feed_category_id
     remove_index :feed_categories, :column => :id
 
