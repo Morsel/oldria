fbfe93a634d7698c1a434b8e356466d82a69980c
Joshua Davey <josh@neotericdesign.com>
2010-02-09 11:47:50 -0600

    Fixing gem dependencies, with explicit versions

--- config/environment.rb
+++ config/environment.rb
@@ -7,26 +7,27 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'will_paginate'
-  config.gem 'RedCloth', :lib => 'redcloth'
-  config.gem 'searchlogic'
-  config.gem 'authlogic'
-  config.gem 'addresslogic'
-  config.gem 'cancan'
-  config.gem 'paperclip'
+  config.gem 'will_paginate',         :version => "~>2.3.9"
+  config.gem 'RedCloth',              :lib => 'redcloth'
+  config.gem 'searchlogic',           :version => "~>2.4.5"
+  config.gem 'authlogic',             :version => "~>2.1.3"
+  config.gem 'addresslogic',          :version => "~>1.2.1"
+  config.gem 'cancan',                :version => "~>1.0.1"
+  config.gem 'paperclip',             :version => "~>2.3.0"
   config.gem 'moomerman-twitter_oauth', :lib => 'twitter_oauth', :source => "http://gems.github.com"
   config.gem 'jackdempsey-acts_as_commentable', :lib => 'acts_as_commentable', :source => "http://gems.github.com"
-  config.gem 'acts_as_list'
-  config.gem 'aasm', :source => 'http://gemcutter.org'
-  config.gem 'formtastic', :source => "http://gemcutter.org"
-  config.gem 'tabletastic', :source => "http://gemcutter.org", :version => ">=0.1.2"
-  config.gem 'validation_reflection'
-  config.gem 'friendly_id'
-  config.gem 'feedzirra'
-  config.gem 'whenever', :lib => false
-  config.gem 'delayed_job'
+  config.gem 'acts_as_list',          :version => "~>0.1.2"
+  config.gem 'aasm',                  :version => "~>2.1.5"
+  config.gem 'validation_reflection', :version => "~>0.3.5"
+  config.gem 'formtastic',            :version => "~>0.9.7"
+  config.gem 'tabletastic',           :version => "~>0.1.2" # Versions at or above 0.1.2, but below 0.2.0
+  config.gem 'friendly_id',           :version => "~>2.2.7" # Versions at or above 2.2.7, but below 2.3.0
+  config.gem 'loofah',                :version => "~>0.4.6"
+  config.gem 'feedzirra',             :version => "~>0.0.20"
+  config.gem 'whenever',              :lib => false
+  config.gem 'delayed_job',           :version => "~>1.8.4"
 
-  config.gem 'hoptoad_notifier'
+  config.gem 'hoptoad_notifier',      :version => "~>2.1.3"
 
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
