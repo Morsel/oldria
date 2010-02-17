9a33c3542dbce65e7e9c2f49bf389854600a785d
Joshua Davey <josh@neotericdesign.com>
2010-02-17 17:29:28 -0600

    Fixing no entries == no links bug for Admin Messages

--- app/models/admin/message.rb
+++ app/models/admin/message.rb
@@ -14,7 +14,7 @@
 class Admin::Message < ActiveRecord::Base
   set_table_name "admin_messages"
 
-  has_many :admin_conversations, :class_name => 'Admin::Conversation', :foreign_key => 'admin_message_id'
+  has_many :admin_conversations, :class_name => 'Admin::Conversation', :foreign_key => 'admin_message_id', :dependent => :destroy
   has_many :recipients, :through => :admin_conversations
   validates_presence_of :message
 
