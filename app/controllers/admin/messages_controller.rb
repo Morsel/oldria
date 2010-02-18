9d19aa9ac8fef7353acd6bf1fc56dc034dd2ba03
Joshua Davey <josh@neotericdesign.com>
2010-02-18 13:17:48 -0600

    Admin Messaging: By default, sort by date, most recent at top

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -3,7 +3,7 @@ class Admin::MessagesController < Admin::AdminController
   # GET /admin/messages
   # GET /admin/messages.xml
   def index
-    @messages = Admin::Message.all
+    @messages = Admin::Message.all(:order => 'created_at DESC')
     @message_groups = @messages.group_by(&:class)
 
     respond_to do |format|
