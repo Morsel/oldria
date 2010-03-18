bb1f534e10992ce6507855ce673d21ccf1bab990
Joshua Davey <josh@neotericdesign.com>
2010-03-18 18:16:08 -0500

    Add directory to nav and lock it down

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -1,7 +1,11 @@
 class DirectoryController < ApplicationController
+  before_filter :require_user
 
   def index
-    @restaurants = Restaurant.paginate(:order => 'UPPER(name) ASC', :page => params[:page], :include => {:employments => [:employee, :restaurant_role]})
+    @restaurants = Restaurant.paginate(
+                    :order => 'UPPER(name) ASC',
+                    :page => params[:page],
+                    :include => {:employments => [:employee, :restaurant_role]})
   end
 
 end
