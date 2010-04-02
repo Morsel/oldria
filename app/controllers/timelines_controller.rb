8bd520dea45216c7dd3cf7117846cc9f3b1e3f6a
Joshua Davey <josh@neotericdesign.com>
2010-04-02 14:15:50 -0500

    Conditionally display twitter timeline if available

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,22 +1,29 @@
 class TimelinesController < ApplicationController
   before_filter :require_user
   before_filter :get_friend_activity, :except => :twitter
-  
+
+  ##
+  # GET /timelines
   def index
     render :people_you_follow
   end
 
+  ##
+  # GET /timelines/twitter
   def twitter
-    @tweets = current_user.twitter_client.friends_timeline
+    if current_user.twitter_authorized?
+      @tweets = current_user.twitter_client.friends_timeline
+    end
   end
 
+  ##
+  # GET /timelines/people_you_follow
   def people_you_follow
   end
-  
+
   private
-  
+
   def get_friend_activity
     @friend_activity = Status.friends_of_user(current_user).all
   end
-
 end
