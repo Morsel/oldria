91e65ff862f3394359f4fa8ecac85ed1b945a7b9
Joe Sak <joe@neotericdesign.com>
2010-04-05 14:56:44 -0500

    Custom intra nav style added to text editor

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -1,11 +1,11 @@
 CKEDITOR.editorConfig = function(config) {
 config.uiColor = '#E2E2E2';
 config.toolbar_Basic = [
-		['Format'],
+		['Styles'],
 		['Undo', 'Redo'],
 		['Bold', 'Italic', '-', 'NumberedList', 'BulletedList'],
 		['Find','RemoveFormat'],
-		['Link', 'Unlink', '-', 'Source']
+		['Link', 'Unlink', 'Anchor', '-', 'Source']
 	];
 
 config.format_tags = 'p;h2;h3;h4;h5;pre';
