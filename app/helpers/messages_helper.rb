bbb3b76e75f7fab9ed4fdea9fe0bf93f214d7be5
Joshua Davey <josh@neotericdesign.com>
2010-04-12 14:11:50 -0500

    Display Trend Questions like other admin messages

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,14 +1,16 @@
 module MessagesHelper
   def reply_link_for_message(message)
     return unless message
-    if message.admin_message && message.admin_message.respond_to?(:holiday)
-      return unless holiday = message.admin_message.holiday
+    if message.respond_to?(:admin_message) && message.admin_message.respond_to?(:holiday)
+      return unless holiday = message.admin_message.try(:holiday)
       rid = holiday.holiday_conversations.find_by_recipient_id(message.recipient_id)
       link_to "Reply", holiday_conversation_path(rid)
     elsif message.respond_to?(:admin_message)
       link_to "Reply", admin_conversation_path(message)
+    elsif message.respond_to?(:trend_question)
+      link_to "Reply", trend_question_discussion_path(message)
     end
   end
-  
-  
+
+
 end
