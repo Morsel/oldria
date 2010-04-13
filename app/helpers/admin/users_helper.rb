fe31db1dba068415f3d2fa4fc8f01e785599cbe8
Joshua Davey <josh@neotericdesign.com>
2010-04-13 17:24:46 -0500

    Show unconfirmed users as a highlighted row in admin
    http://www.pivotaltracker.com/story/show/3116438

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,9 @@
 module Admin::UsersHelper
+
+  def users_extra_row_class(user)
+    admin_or_not = user.admin? ? 'admin' : ''
+    confirmed_or_unconfirmed = user.confirmed? ? 'confirmed' : 'unconfirmed'
+    "#{admin_or_not} #{confirmed_or_unconfirmed}"
+  end
+
 end
