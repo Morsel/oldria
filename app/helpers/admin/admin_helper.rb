b9e84e2ebd9947cf102464374e46dcdf72530dbd
Joshua Davey <josh@neotericdesign.com>
2010-04-16 15:38:00 -0500

    Some Admin view cleanup

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -3,4 +3,8 @@ module Admin::AdminHelper
     link_to "#{holiday.reply_count} of #{holiday.holiday_discussions.count} replies",
     admin_holiday_path(holiday)
   end
+
+  def reply_count_link(discussionable)
+    link_to "#{discussionable.admin_discussions.with_replies.size} of #{discussionable.admin_discussions.size} replies", [:admin, discussionable]
+  end
 end
