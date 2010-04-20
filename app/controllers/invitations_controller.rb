58bb9e11440a2548535a28c4af942e80708f7f82
Joshua Davey <josh@neotericdesign.com>
2010-04-20 16:12:11 -0500

    Logout a logged-in user before doing invitation stuff

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,15 +1,25 @@
 class InvitationsController < ApplicationController
+  before_filter :logout_current_user
+  before_filter :find_user_from_params
+
   def show
-    if current_user
-      @user = current_user
-      # render show
-    elsif @user = User.find_using_perishable_token(params[:id])
+    if @user
       @user.update_attribute(:confirmed_at, Time.now)
-      @user_session = UserSession.create(@user)
+      UserSession.create(@user)
       flash[:notice] = "Successfully logged in. Please take a moment and update your account information."
     else
       flash[:error] = "We could not locate your account."
       redirect_to root_url
     end
+  end
+
+  private
+
+  def logout_current_user
+    @current_user_session.destroy if current_user_session
+  end
+
+  def find_user_from_params
+    @user = User.find_using_perishable_token(params[:id])
   end
 end
--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -1,42 +1,65 @@
 require 'spec/spec_helper'
 
 describe InvitationsController do
+  before(:each) do
+    @invitee = Factory(:user, :perishable_token =>"abc789", :username => "johnjohn", :confirmed_at => nil)
+  end
 
   describe "GET show" do
-    before(:each) do
-      @user = Factory(:user, :perishable_token =>"abc789", :username => "johnjohn", :confirmed_at => nil)
-    end
+    context "when no one is logged in" do
+      context "user is found" do
+        before do
+          User.expects(:find_using_perishable_token).with("abc789").returns(@invitee)
+          get :show, :id => "abc789"
+        end
+
+        it { assigns[:user].should eql(@invitee) }
+        it { should render_template(:show) }
+
+        it "should flash a notice" do
+          flash[:notice].should_not be_nil
+        end
+
+        it "should confirm the user" do
+          @invitee.should be_confirmed
+        end
+      end
     
-    context "user is found" do
-      before do
-        User.expects(:find_using_perishable_token).with("abc789").returns(@user)
-        get :show, :id => "abc789"
-      end
+      context "user is not found" do
+        before do
+          User.expects(:find_using_perishable_token).with("abc789")
+          get :show, :id => "abc789"
+        end
 
-      it { assigns[:user].should eql(@user) }
-      it { should render_template(:show) }
+        it { assigns[:user].should be_nil }
+        it { should redirect_to(root_url) }
 
-      it "should flash a notice" do
-        flash[:notice].should_not be_nil
-      end
-
-      it "should confirm the user" do
-        @user.should be_confirmed
+        it "should flash an error message" do
+          flash[:error].should_not be_nil
+        end
       end
     end
-    
-    context "user is not found" do
-      before do
-        User.expects(:find_using_perishable_token).with("abc789")
-        get :show, :id => "abc789"
-      end
 
-      it { assigns[:user].should be_nil }
-      it { should redirect_to(root_url) }
-
-      it "should flash and error message" do
-        flash[:error].should_not be_nil
+    context "when a user is already logged in" do
+      context "and is the invitee" do
+        it "should redirect to the dashboard" do
+          get :show, :id => 'expired_id', :user_id => @invitee.id
+          response.should redirect_to(root_path)
+        end
       end
     end
-  end
+
+  end # GET show
 end
+
+=begin
+
+If I click through an invitation email link, and another user is logged in, that user should be logged out.  *I* should be logged in instead, then view the "short registration" page.  
+
+If I try to save the short confirmation page without specifying a password, I should see the standard rails error. 
+
+If I click thru the invitation email link, and no other user is logged in, I should be logged in automatically. 
+
+If I click thru the invitation email link 2x, after doing the required actions, I should be logged in automatically. 
+
+=end
