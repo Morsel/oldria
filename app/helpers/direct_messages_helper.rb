0f2d6e8795410e8d66b16b94f75ea558ddfd1578
Joshua Davey <josh@neotericdesign.com>
2010-04-30 12:11:44 -0500

    WIP: List view Private Messages UI

--- app/helpers/direct_messages_helper.rb
+++ app/helpers/direct_messages_helper.rb
@@ -1,5 +1,5 @@
 module DirectMessagesHelper
-  
+
   def details_for(dm)
     "<span>#{date_for(dm.created_at)}</span> <span>#{dm.sender.name_or_username}</span> #{truncate(dm.body, :length => 100)}"
   end
@@ -10,6 +10,23 @@ module DirectMessagesHelper
     else
       "display: none;"
     end
+  end
+
+  def title_for_direct_message(direct_message)
+    return unless direct_message
+    if direct_message.sender_id == current_user.try(:id)
+      "You sent a message to #{link_to direct_message.receiver.name_or_username, profile_path(direct_message.receiver.username)}"
+    else
+      "#{link_to direct_message.sender.name_or_username, profile_path(direct_message.sender.username)} sent you a message"
+    end
+  end
+
+  def css_classes_for_direct_message(direct_message)
+    return unless direct_message
+    classes = "clearfix"
+    classes << ' sent_message' if direct_message.from?(current_user)
+    classes << (direct_message.read_by?(current_user) ? ' read' : ' unread')
+    classes
   end
 
 end
