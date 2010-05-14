cc01244158acfe183fcc212129b58037d52a73cc
Audrey Eschright <spinnerin@gmail.com>
2010-05-14 16:53:07 -0700

    Fixing email notification titles to use new email_title and doing the same to comment emails.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -49,7 +49,7 @@ class UserMailer < ActionMailer::Base
     from        'notifications@restaurantintelligenceagency.com'
     recipients  recipient.email
     sent_on     Time.now
-    subject     "SpoonFeed: #{message.inbox_title} notification"
+    subject     "SpoonFeed: #{message.email_title} notification"
     body        :message => message, :recipient => recipient, :sender => sender
   end
 
