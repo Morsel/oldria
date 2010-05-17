c8421799136c48642b0b4f39c823227138b8a781
Joshua Davey <josh@neotericdesign.com>
2010-05-17 16:47:09 -0500

    Fixing direct message responses count

--- app/models/direct_message.rb
+++ app/models/direct_message.rb
@@ -50,7 +50,7 @@ class DirectMessage < ActiveRecord::Base
   def inbox_title
     self.class.title
   end
-  
+
   def email_title
     inbox_title
   end
@@ -81,6 +81,14 @@ class DirectMessage < ActiveRecord::Base
       message = message.parent_message
     end
     return message
+  end
+
+  def descendants
+    self.responses.map {|child| child.descendants}.flatten + [self]
+  end
+
+  def descendants_size
+    descendants.size - 1 # ignore self for the count
   end
 
   def from?(user)
