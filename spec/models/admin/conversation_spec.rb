f4bf3cf426ee31dd979bcc18a4ed5dd20d93abcd
Joshua Davey <josh@neotericdesign.com>
2010-05-17 11:40:55 -0500

    Fixing links for QOTDs emails

--- spec/models/admin/conversation_spec.rb
+++ spec/models/admin/conversation_spec.rb
@@ -29,9 +29,9 @@ describe Admin::Conversation do
     user = Factory(:user, :prefers_receive_email_notifications => true)
     employment = Factory(:employment, :employee => user)
     message = Factory(:qotd, :scheduled_at => Time.now)
-    UserMailer.expects(:send_at).with(message.scheduled_at, :deliver_message_notification, message, user)
-    Admin::Conversation.create!(Factory.attributes_for(:admin_conversation, :recipient => employment, 
-      :admin_message => message))
+    admin_conversation = Admin::Conversation.new(Factory.attributes_for(:admin_conversation, :recipient => employment, :admin_message => message))
+    admin_conversation.expects(:send_at).with(message.scheduled_at, :queued_message_sending)
+    admin_conversation.save!
   end
 
 end
