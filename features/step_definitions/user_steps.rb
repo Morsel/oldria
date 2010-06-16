8c2b2d05cc0d4691240ad441626f7deee3d79f98
Audrey Eschright <spinnerin@gmail.com>
2010-06-16 16:15:38 -0700

    Adding cucumber steps to check login errors.

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -8,6 +8,12 @@ Given /^the following confirmed users?:?$/ do |table|
   end
 end
 
+Given /^the following unconfirmed users?:?$/ do |table|
+  table.hashes.each do |row|
+    user = Factory(:user, row.merge(:confirmed_at => nil))
+  end
+end
+
 Given /^the following media users?:?$/ do |table|
   table.hashes.each do |row|
     user = Factory(:media_user, row)
