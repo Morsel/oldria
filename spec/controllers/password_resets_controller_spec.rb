4f48ce531789ff18ac10357540400f45dc52f10f
Audrey Eschright <spinnerin@gmail.com>
2010-06-18 10:24:33 -0700

    Preventing users with unconfirmed accounts from resetting their password, and directing them to get a new confirmation email instead.

---
+++ spec/controllers/password_resets_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec/spec_helper'
+
+describe PasswordResetsController do
+
+  it "should reset a password for a user when valid email is provided" do
+    user = Factory(:user)
+    User.stubs(:find_by_email).returns(user)
+    user.expects(:deliver_password_reset_instructions!)
+    post :create, :email => user.email
+  end
+  
+  it "should not send the password reset email when a user attempts to reset an unconfirmed account" do
+    user = Factory(:user, :confirmed_at => nil)
+    User.stubs(:find_by_email).returns(user)
+    user.expects(:deliver_password_reset_instructions!).never
+    post :create, :email => user.email
+  end
+
+end
