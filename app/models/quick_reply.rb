a787f31d5bfbc4b81a0a5b122c414d9d81486d6a
Joshua Davey <josh@neotericdesign.com>
2010-06-25 12:36:25 -0500

    Basic messaging to multiples working

--- app/models/quick_reply.rb
+++ app/models/quick_reply.rb
@@ -10,24 +10,38 @@ class QuickReply < ActiveRecord::Base
   column :reply_text, :text
   column :user_id, :integer
 
+  validates_presence_of :reply_text, :message_id, :message_type, :user_id
   validates_length_of :reply_text, :maximum => 500
 
   belongs_to :message, :polymorphic => true
   belongs_to :user
-  has_many :restaurants
+
+  attr_accessor :restaurant_ids
 
   def save
     # This is where the action is!
     return false unless valid?
-    debugging_info
+
+    discussions.each do |discussion|
+      discussion.comments.create(:comment => reply_text, :user_id => user_id)
+    end
+
+#    debugging_info
     true
+  end
+
+  def discussions
+    return [] unless restaurant_ids
+    @discussions ||= restaurant_ids.map do |restaurant_id|
+      message.admin_discussions.find_by_restaurant_id(restaurant_id)
+    end.compact
   end
 
 
   def debugging_info
     Rails.logger.info <<-EOT
 
-    restaurant_ids = #{restaurant_ids}
+    discussions = #{discussions.join(' ')}
     user = #{user.name}
     message = #{message_type} #{message_id}
 
