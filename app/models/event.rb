2d37278144ab82df8d9ed3a5da9562bb383344d7
Audrey Eschright <spinnerin@gmail.com>
2010-07-07 17:37:08 -0700

    Verifying that events don't end before they start.

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,6 +12,8 @@ class Event < ActiveRecord::Base
   validates_presence_of :title, :start_at, :end_at, :location, :category
   validates_presence_of :status, :if => Proc.new { |event| event.category == "Private" }
   
+  validate :end_comes_after_start
+  
   named_scope :for_month_of, lambda { |date| 
     { :conditions => { :start_at => date.beginning_of_month.at_midnight..date.end_of_month.end_of_day } } 
   }
@@ -48,6 +50,14 @@ class Event < ActiveRecord::Base
   def children
     unless self.restaurant_id
       Event.all(:conditions => { :parent_id => self.id })
+    end
+  end
+  
+  protected
+  
+  def end_comes_after_start
+    if start_at && end_at && end_at < start_at
+      errors.add(:end_at, "The end time must be after the start time")
     end
   end
   
