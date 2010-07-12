a6267469b37450be07d8e8f7b07169428a50e89a
Joshua Davey <josh@neotericdesign.com>
2010-07-12 11:17:57 -0500

    Making Admin and Public/RIA calendars consistent, removing duplication

--- app/controllers/admin/calendars_controller.rb
+++ app/controllers/admin/calendars_controller.rb
@@ -1,12 +1,14 @@
 class Admin::CalendarsController < ApplicationController
-  
+
   def index
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @categories = Event::ADMIN_CATEGORIES
     if params[:category] && (params[:category] != "all")
       @events = Event.for_month_of(@date).by_category(params[:category])
     else
       @events = Event.for_month_of(@date).from_ria
-    end  
+    end
+    render :template  => 'calendars/index'
   end
-  
+
 end
--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -1,2 +1,29 @@
 module CalendarsHelper
+
+  def event_link(event)
+    if admin_calendars?
+      admin_event_path(event)
+    elsif @restaurant && ria_events?
+      ria_details_restaurant_event_path(:restaurant_id => @restaurant.id, :id => event.id)
+    elsif @restaurant
+      restaurant_event_path(:restaurant_id => @restaurant.id, :id => event.id)
+    end
+  end
+
+  def restaurant_calendar_link(restaurant, date = nil)
+    if restaurant && ria_events?
+      ria_restaurant_calendars_path(:restaurant_id => restaurant, :date => date.try(:to_s))
+    elsif restaurant
+      restaurant_calendars_path(:restaurant_id => restaurant, :date => date.try(:to_s))
+    end
+  end
+
+  def ria_events?
+    action_name == 'ria'
+  end
+
+  def admin_calendars?
+    params[:controller] == "admin/calendars"
+  end
+
 end
