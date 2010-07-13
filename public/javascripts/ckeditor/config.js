4aaccdf98b74925e19e46bd66874db05472a7748
Joshua Davey <josh@neotericdesign.com>
2010-07-13 14:42:20 -0500

    New CKEditor version and plugin

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -1,43 +1,50 @@
-CKEDITOR.editorConfig = function(config) {
-config.uiColor = '#E2E2E2';
-config.toolbar_Basic = [
-		['Styles'],
-		['Undo', 'Redo'],
-		['Bold', 'Italic', '-', 'NumberedList', 'BulletedList'],
-		['Find','RemoveFormat'],
-		['Link', 'Unlink', 'Anchor', '-', 'Source']
-	];
+﻿CKEDITOR.editorConfig = function( config )
+{
+    config.uiColor = '#E2E2E2';
+    config.toolbar_Basic = [
+        ['Styles'],
+        ['Undo', 'Redo'],
+        ['Bold', 'Italic', '-', 'NumberedList', 'BulletedList'],
+        ['Find','RemoveFormat'],
+        ['Link', 'Unlink', 'Anchor', '-', 'Source'],
+        ['Image','Embed','Attachment']
+    ];
 
-config.format_tags = 'p;h2;h3;h4;h5;pre';
+    config.PreserveSessionOnFileBrowser = true;
+    config.language = 'en';
+    config.format_tags = 'p;h2;h3;h4;h5;pre';
 
-config.forcePasteAsPlainText = true;
-config.toolbar = 'Basic';
-config.resize_maxWidth = 933;
-config.resize_minWidth = 639;
+    config.forcePasteAsPlainText = true;
+    config.toolbar = 'Basic';
+    config.resize_maxWidth = 933;
+    config.resize_minWidth = 639;
+
+    config.extraPlugins = "embed,attachment";
 };
 
 // HTML writer properties
 CKEDITOR.on( 'instanceReady', function(ev){
-	var htmlWriter = ev.editor.dataProcessor.writer;
-	var formattingDefaults = {
-		indent : false,
-		breakBeforeOpen : true,
-		breakAfterOpen : false,
-		breakBeforeClose : false,
-		breakAfterClose : true
-	};
-	htmlWriter.indentationChars = "  ";
-	htmlWriter.lineBreakChars = "\n\n";
-	htmlWriter.setRules('p', formattingDefaults);
-	htmlWriter.setRules('h2', formattingDefaults);
-	htmlWriter.setRules('h3', formattingDefaults);
-	htmlWriter.setRules('h4', formattingDefaults);
-	htmlWriter.setRules('div', {
-		indent : true,
-		breakBeforeOpen : true,
-		breakAfterOpen : true,
-		breakBeforeClose : true,
-		breakAfterClose : true
-	});
+    var htmlWriter = ev.editor.dataProcessor.writer;
+    var formattingDefaults = {
+        indent : false,
+        breakBeforeOpen : true,
+        breakAfterOpen : false,
+        breakBeforeClose : false,
+        breakAfterClose : true
+    };
+    htmlWriter.indentationChars = "  ";
+    htmlWriter.lineBreakChars = "\n\n";
+    htmlWriter.setRules('p', formattingDefaults);
+    htmlWriter.setRules('h2', formattingDefaults);
+    htmlWriter.setRules('h3', formattingDefaults);
+    htmlWriter.setRules('h4', formattingDefaults);
+    htmlWriter.setRules('div', {
+        indent : true,
+        breakBeforeOpen : true,
+        breakAfterOpen : true,
+        breakBeforeClose : true,
+        breakAfterClose : true
+    });
 });
+
 
