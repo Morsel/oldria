ad55b7a92941fec80e9565b388d4fc5bac103e19
Audrey Eschright <spinnerin@gmail.com>
2010-07-23 11:40:43 -0700

    Adding sidebar content to soapbox single item view.

--- app/controllers/soapbox_controller.rb
+++ app/controllers/soapbox_controller.rb
@@ -1,5 +1,7 @@
 class SoapboxController < ApplicationController
 
+  before_filter :load_past_features
+
   def index
     @main_feature = SoapboxEntry.main_feature
     @main_feature_comments = SoapboxEntry.main_feature_comments if @main_feature
@@ -7,14 +9,20 @@ class SoapboxController < ApplicationController
     @secondary_feature = SoapboxEntry.secondary_feature
     @secondary_feature_comments = SoapboxEntry.secondary_feature_comments if @secondary_feature
 
-    @qotds = SoapboxEntry.qotd.published.recent.all(:include => :featured_item).map(&:featured_item)
-    @trend_questions = SoapboxEntry.trend_question.published.recent.all(:include => :featured_item).map(&:featured_item)
+    load_past_features
   end
 
   def show
     entry = SoapboxEntry.find(params[:id], :include => :featured_item)
     @feature = entry.featured_item
     @feature_comments = entry.comments
+  end
+  
+  protected
+  
+  def load_past_features
+    @qotds = SoapboxEntry.qotd.published.recent.all(:include => :featured_item).map(&:featured_item)
+    @trend_questions = SoapboxEntry.trend_question.published.recent.all(:include => :featured_item).map(&:featured_item)
   end
 
 end
