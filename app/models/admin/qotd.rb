c7002f185c6ed851d9ad630c5854be8cb4924150
Audrey Eschright <spinnerin@gmail.com>
2010-07-23 10:36:28 -0700

    Linking admin QOTD to soapbox entry if one exists.

--- app/models/admin/qotd.rb
+++ app/models/admin/qotd.rb
@@ -14,6 +14,10 @@
 #
 
 class Admin::Qotd < Admin::Message
+  
+  # TODO - figure out why :as => :featured_item isn't working here
+  has_one :soapbox_entry, :foreign_key => :featured_item_id, :conditions => { :featured_item_type => "Admin::Qotd" }
+  
   def self.title
     "Question of the Day"
   end
