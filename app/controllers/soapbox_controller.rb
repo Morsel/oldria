338cb2508c9d31e981a84cd994588ff3ce827360
Audrey Eschright <spinnerin@gmail.com>
2010-07-22 17:33:50 -0700

    Adding soapbox content finders and stubbing view for testing.

--- app/controllers/soapbox_controller.rb
+++ app/controllers/soapbox_controller.rb
@@ -1,10 +1,14 @@
 class SoapboxController < ApplicationController
 
   def index
-    @main_soapbox_entry = SoapboxEntry.trend_question.first(:include => :featured_item)
-    @featured_trend_question = @main_soapbox_entry.try(:featured_item)
+    @featured_trend_question = SoapboxEntry.featured_trend_question
+    @featured_tq_comments = SoapboxEntry.featured_trend_question_comments if @featured_trend_question
+    
+    @featured_qotd = SoapboxEntry.featured_qotd
+    @featured_qotd_comments = SoapboxEntry.featured_qotd_comments if @featured_qotd
 
-    @qotds = SoapboxEntry.qotd.all(:include => :featured_item)
+    @qotds = SoapboxEntry.qotd.published.recent.all(:include => :featured_item).map(&:featured_item)
+    @trend_questions = SoapboxEntry.trend_question.published.recent.all(:include => :featured_item).map(&:featured_item)
   end
 
 end
--- app/models/soapbox_entry.rb
+++ app/models/soapbox_entry.rb
@@ -14,11 +14,37 @@
 class SoapboxEntry < ActiveRecord::Base
   belongs_to :featured_item, :polymorphic => true
 
-  named_scope :qotd, :conditions => {:featured_item_type => 'Admin::Qotd'}
-  named_scope :trend_question, :conditions => {:featured_item_type => 'TrendQuestion'}
+  named_scope :qotd, :conditions => { :featured_item_type => 'Admin::Qotd' }
+  named_scope :trend_question, :conditions => { :featured_item_type => 'TrendQuestion' }
+  
+  named_scope :published, 
+              :conditions => ['published_at < ?', Time.zone.now], 
+              :order => "published_at desc"
+              
+  named_scope :recent,
+              :limit => 5,
+              :offset => 1,
+              :order => "published_at desc"
 
   def title
     featured_item.title
+  end
+  
+  def self.featured_trend_question
+    trend_question.published.first(:include => :featured_item).try(:featured_item)
+  end
+  
+  def self.featured_trend_question_comments
+    # FIXME: filter to authorized users/employments only - rejecting admins because they break the comment partial
+    self.featured_trend_question.comments(true).reject { |c| c.user.admin? }
+  end
+  
+  def self.featured_qotd
+    qotd.published.first(:include => :featured_item).try(:featured_item)
+  end
+  
+  def self.featured_qotd_comments
+    self.featured_qotd.conversations_with_replies.map(&:comments).flatten.reject { |c| c.user.admin? }
   end
 
 end
