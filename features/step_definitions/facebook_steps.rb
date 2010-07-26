29a4717ec285aeb5356142f6cb388a57bda1f091
Audrey Eschright <spinnerin@gmail.com>
2010-07-26 10:09:54 -0700

    Stubbing current_facebook_user for cucumber.

--- features/step_definitions/facebook_steps.rb
+++ features/step_definitions/facebook_steps.rb
@@ -2,4 +2,6 @@ Given /^Facebook is functioning$/ do
   status = JSON.parse( File.new( RAILS_ROOT + '/spec/fixtures/facebook_response.json').read )
   Mogli::User.stubs(:new).returns(user = mock())
   user.stubs(:feed_create).returns(status)
+
+  StatusesController.any_instance.stubs(:current_facebook_user).returns(user)
 end
