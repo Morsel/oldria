e454c1ad80ff4859d23f8e6ac4e332a242479093
Joshua Davey <josh@neotericdesign.com>
2010-08-05 14:56:23 -0500

    Annotating new Profile models

--- spec/models/extended_profile_item_spec.rb
+++ spec/models/extended_profile_item_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+# Schema version: 20100805194513
+#
+# Table name: extended_profile_items
+#
+#  id         :integer         not null, primary key
+#  profile_id :integer
+#  category   :string(255)
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe ExtendedProfileItem do
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+# Schema version: 20100805194513
+#
+# Table name: profiles
+#
+#  id         :integer         not null, primary key
+#  user_id    :integer         not null
+#  birthday   :date
+#  job_start  :date
+#  cellnumber :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Profile do
