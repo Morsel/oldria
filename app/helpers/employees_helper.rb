b85ff87acc1adcf4aa59a02d931f8f3dad062d56
Joshua Davey <josh@neotericdesign.com>
2010-08-10 13:44:49 -0500

    Show user identities consistently as with profile

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -2,8 +2,8 @@ module EmployeesHelper
   def restaurant_role_and_link(employment)
     role_name = employment.restaurant_role.try(:name) || "<em>No Role Assigned</em>"
     delete_link = link_to('<span class="delete">Delete</span>',
-                        restaurant_employee_path(employment.restaurant, employment.employee), 
-                        :method => :delete, 
+                        restaurant_employee_path(employment.restaurant, employment.employee),
+                        :method => :delete,
                         :confirm => 'Are you sure?',
                         :title => 'Delete')
     edit_link = link_to('<span class="edit">edit</span>',
@@ -13,4 +13,29 @@ module EmployeesHelper
       "#{role_name} #{delete_link} #{edit_link}"
     end
   end
+
+
+  def identity_details_list(employment, highlight_restaurant = false)
+    list_items = [
+      content_tag(:li, :class => "employee_name#{' primary' unless highlight_restaurant}") do
+        employment.employee.name_or_username
+      end,
+
+      content_tag(:li, :class => 'employee_role') { employment.try(:restaurant_role).try(:name)},
+
+      content_tag(:li, :class => "restaurant_name#{' primary' if highlight_restaurant}") do
+        employment.try(:restaurant).try(:name)
+      end
+    ]
+
+    if highlight_restaurant
+      restaurant_item = list_items.pop
+      list_items.unshift(restaurant_item)
+    end
+
+    list_class = highlight_restaurant ? 'restaurant' : 'user'
+
+    content_tag(:ul, list_items.join(""), :class => "identity_details #{list_class}")
+  end
+
 end
