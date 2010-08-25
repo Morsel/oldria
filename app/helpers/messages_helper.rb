11ca89f35c505132eb3959a8f646458423ca4527
Audrey Eschright <spinnerin@gmail.com>
2010-08-24 17:35:20 -0700

    Updating reply links for trend questions and content requests to better use "quick post" interface and allow separate restaurant discussions to be viewed.

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -52,7 +52,7 @@ module MessagesHelper
     elsif message.respond_to?(:admin_message)
       link_path = read_admin_message_path(message.admin_message)
     elsif message.respond_to?(:admin_discussions) # TrendQuestion or ContentRequest
-      first_discussion_for_user = current_user.unread_grouped_admin_discussions[message].first
+      first_discussion_for_user = current_user.grouped_admin_discussions[message].first
       link_path = read_admin_discussion_path(first_discussion_for_user)
     else
       link_path = read_admin_message_path(message)
