11b8d95924b7caf4e2e733c80fd46522cf59ffd5
Audrey Eschright <spinnerin@gmail.com>
2010-09-02 17:41:00 -0700

    Adding profile cuisine cucumber steps.

---
+++ features/profile/profile_cuisines.feature
@@ -0,0 +1,15 @@
+@profile
+Feature: Profile Cuisines
+  Users may select one or more cuisines to appear on their profile
+  
+  Background:
+  Given a cuisine named "Fondue"
+  
+  Scenario: Adding a cuisine to your profile
+  Given I am logged in as a normal user
+  And I am on my profile's edit page
+  When I add a cuisine to my profile with:
+    | Cuisine   | Fondue    |
+  Then I should have 1 cuisine on my profile
+  And I should see "Fondue" on my profile page
+  
--- features/step_definitions/profile_extended_steps.rb
+++ features/step_definitions/profile_extended_steps.rb
@@ -1,8 +1,8 @@
 def fill_in_fields_for_table(table)
   table.rows_hash.each do |field, value|
-    if field == "Start date"
+    if field == "Start date" || field == "Run date"
       select_date(Date.parse(value))
-    elsif field == 'Media Type'
+    elsif field == 'Media Type' || field == 'Cuisine'
       select value, :from => field
     else
       fill_in field, :with => value
@@ -39,6 +39,14 @@ When /^I add a culinary school to my profile with:$/ do |table|
   When %Q{I add a profile item "culinary_school" to my profile with:}, table
 end
 
+When /^I add a cuisine to my profile with:$/ do |table|
+  When %Q{I add a profile item "cuisine" to my profile with:}, table
+end
+
+Given /^a cuisine named "([^\"]*)"$/ do |cuisine|
+  Factory(:cuisine, :name => cuisine)
+end
+
 Then /^I should have (\d+) culinary schools? on my profile$/ do |num|
   @current_user.profile.schools.count.should == num.to_i
 end
@@ -65,6 +73,10 @@ end
 
 Then(/^I should have (\d+) awards? on my profile$/) do |num|
   @current_user.profile.awards.count.should == num.to_i
+end
+
+Then(/^I should have (\d+) cuisines? on my profile$/) do |num|
+  @current_user.profile.cuisines.count.should == num.to_i
 end
  
 Then(/^I should see "([^"]*)" on my profile page$/) do |text|
