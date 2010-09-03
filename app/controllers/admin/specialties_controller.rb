bbc9d95611b451c537768ccf64c18264fd892e61
Audrey Eschright <spinnerin@gmail.com>
2010-09-03 15:51:00 -0700

    Adding admin interface for specialties (index, new, edit).

--- app/controllers/admin/specialties_controller.rb
+++ app/controllers/admin/specialties_controller.rb
@@ -4,4 +4,32 @@ class Admin::SpecialtiesController < ApplicationController
     @specialties = Specialty.all
   end
   
+  def new
+    @specialty = Specialty.new
+  end
+  
+  def create
+    @specialty = Specialty.new(params[:specialty])
+    if @specialty.save
+      flash[:notice] = "Created specialty \"#{@specialty.name}\""
+      redirect_to :action => "index"
+    else
+      render :action => "new"
+    end
+  end
+  
+  def edit
+    @specialty = Specialty.find(params[:id])
+  end
+  
+  def update
+    @specialty = Specialty.find(params[:id])
+    
+    if @specialty.update_attributes(params[:specialty])
+      flash[:notice] = "Updated specialty \"#{@specialty.name}\""
+      redirect_to :action => "index"
+    else
+      render :action => "edit"
+    end
+  end
 end
