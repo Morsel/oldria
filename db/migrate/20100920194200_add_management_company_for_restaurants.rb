a78cb9dbbee8fddea481d03b482f7ac256420063
Noel Rappin <noelrappin@gmail.com>
2010-09-20 14:49:19 -0500

    More basic restaurant features

---
+++ db/migrate/20100920194200_add_management_company_for_restaurants.rb
@@ -0,0 +1,11 @@
+class AddManagementCompanyForRestaurants < ActiveRecord::Migration
+  def self.up
+    add_column :restaurants, :management_company_name, :string
+    add_column :restaurants, :management_company_website, :string
+  end
+
+  def self.down
+    remove_column :restaurants, :management_company_name
+    remove_column :restaurants, :management_company_website
+  end
+end
