f029f569816fa078ae0e4d0c13c104f4987068ed
Andy Maleh <andy@obtiva.com>
2010-09-22 15:51:39 -0500

    Upload restaurant photos (story id 5228029)

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -109,6 +109,8 @@ module NavigationHelpers
       edit_photos_admin_restaurant_path(Restaurant.find_by_name($1))
     when /^the admin restaurant feature page$/
       admin_restaurant_features_path
+    when /^the restaurant photo upload page for (.+)$/
+      edit_photos_admin_restaurant_path(Restaurant.find_by_name($1))
 
     # Direct path
     when /"([^\"]+)"/
