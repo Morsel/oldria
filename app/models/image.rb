5bfe1e8eb7da2451cfa34d901ee3dee72e9a1c80
Andy Maleh <andy@obtiva.com>
2010-09-22 15:51:39 -0500

    Upload restaurant photos (story id 5228029)

---
+++ app/models/image.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: attachments
+#
+#  id                      :integer         not null, primary key
+#  attachment_file_name    :string(255)
+#  attachment_content_type :string(255)
+#  attachment_file_size    :integer
+#  attachment_updated_at   :datetime
+#  attachable_id           :integer
+#  attachable_type         :string(255)
+#  created_at              :datetime
+#  updated_at              :datetime
+#
+
+class Image < Attachment
+  has_attached_file :attachment,
+    :storage => :s3,
+    :s3_credentials => "#{RAILS_ROOT}/config/environments/#{RAILS_ENV}/amazon_s3.yml",
+    :path => "#{RAILS_ENV}/images/:id/:style/:filename",
+    :bucket => "spoonfeed",
+    :url => ':s3_domain_url'
+end
