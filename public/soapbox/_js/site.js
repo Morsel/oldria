2b12b4ef8b6706fa3d870d6150e9f47727171d0b
Joe Sak <joe@joesak.com>
2010-10-13 17:52:12 -0500

    Jumbotron smoothed out, IE html5'd and png bg added where rgba is

--- public/soapbox/_js/site.js
+++ public/soapbox/_js/site.js
@@ -1,36 +1,42 @@
 $(document).ready(function(){
 	var panelHeight = homePanelHeight = 0;
 	
-	$('#recent-activity').tabs({
-		panelTemplate: '<ol></ol>',
-		fx: { duration: 'fast', opacity: 'toggle' }
-	}).find('ol').each(function(){	
-		if($(this).height() > panelHeight){
-			panelHeight = $(this).height();
-		}
-	}).each(function(){
-		$(this).height(panelHeight +'px');
-	});
-
-	$('#inside').not('.home').height($('#main').height() - 50 + 'px');
+	if($('#recent-activity').length > 0){
 	
-	$('#content.home .panel').each(function(){
-		if($(this).height() > homePanelHeight){
-			homePanelHeight = $(this).height();
-		}
-	}).each(function(){
-		$(this).height(homePanelHeight +'px');
-	});
-	
-	$('#cycle').cycle({
-		timeout: 8000,
-		fx: 'uncover',
-		easing: 'easeOutCubic',
-		pager: '#pager',
-		pagerAnchorBuilder: buildPager,
-		after: displayInfo
-	});
+		$('#recent-activity').tabs({
+			panelTemplate: '<ol></ol>',
+			fx: { duration: 'fast', opacity: 'toggle' }
+		}).find('ol').each(function(){	
+			if($(this).height() > panelHeight){
+				panelHeight = $(this).height();
+			}
+		}).each(function(){
+			$(this).height(panelHeight +'px');
+		});
 
+		$('#inside').not('.home').height($('#main').height() - 50 + 'px');
+	}
+	
+	if($('#content.home .panel').length > 1){
+		$('#content.home .panel').each(function(){
+			if($(this).height() > homePanelHeight){
+				homePanelHeight = $(this).height();
+			}
+		}).each(function(){
+			$(this).height(homePanelHeight +'px');
+		});
+	}
+	
+	if($('#cycle').length > 0){
+		$('#cycle').cycle({
+			timeout: 8000,
+			fx: 'uncover',
+			easing: 'easeInOutCubic',
+			pager: '#pager',
+			pagerAnchorBuilder: buildPager,
+			after: displayInfo
+		});
+	}
 });
 	
 function buildPager(idx, elem){
