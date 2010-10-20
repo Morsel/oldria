75e78de35b4320c4232bea94cd64ae8625c4a390
Audrey Eschright <spinnerin@gmail.com>
2010-10-19 17:29:02 -0700

    Cleanup.

--- app/controllers/soapbox/soapbox_controller.rb
+++ app/controllers/soapbox/soapbox_controller.rb
@@ -12,8 +12,6 @@ class Soapbox::SoapboxController < ApplicationController
   end
 
   def directory
-    # @restaurants_and_employments = Employment.all(:include => :restaurant, 
-    #   :order => "restaurants.name ASC").select { |e| e.employee.prefers_publish_profile? }.group_by(&:restaurant)
     directory_search_setup
     @use_search = true
     render :template => "directory/index"
