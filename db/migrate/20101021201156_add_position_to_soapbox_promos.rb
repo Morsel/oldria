d39da7b688417cedd9229acd8bb43e5c7c401bca
Audrey Eschright <spinnerin@gmail.com>
2010-10-21 13:15:57 -0700

    Admins can edit and sort soapbox promos.

---
+++ db/migrate/20101021201156_add_position_to_soapbox_promos.rb
@@ -0,0 +1,9 @@
+class AddPositionToSoapboxPromos < ActiveRecord::Migration
+  def self.up
+    add_column :soapbox_promos, :position, :integer
+  end
+
+  def self.down
+    remove_column :soapbox_promos, :position
+  end
+end
