fb091a235a03c5bee5f7a4ac257bfa47e4c182a8
Nick Gracilla <nick@neotericdesign.com>
2010-10-25 13:40:11 -0500

    revised HP soapbox fb like interface

--- db/schema.rb
+++ db/schema.rb
@@ -190,21 +190,19 @@ ActiveRecord::Schema.define(:version => 20101022194902) do
   end
 
   create_table "culinary_jobs", :force => true do |t|
-    t.integer  "profile_id",                         :null => false
-    t.string   "restaurant_name", :default => "",    :null => false
-    t.string   "title",           :default => "",    :null => false
-    t.string   "city",            :default => "",    :null => false
-    t.string   "state",           :default => "",    :null => false
-    t.string   "country",         :default => "",    :null => false
-    t.date     "date_started",                       :null => false
+    t.integer  "profile_id",                       :null => false
+    t.string   "title",         :default => "",    :null => false
+    t.string   "city",          :default => "",    :null => false
+    t.string   "state",         :default => "",    :null => false
+    t.date     "date_started",                     :null => false
     t.date     "date_ended"
-    t.string   "chef_name",       :default => "",    :null => false
-    t.boolean  "chef_is_me",      :default => false, :null => false
-    t.text     "cuisine",         :default => "",    :null => false
-    t.text     "notes",           :default => "",    :null => false
+    t.string   "chef_name",     :default => "",    :null => false
+    t.boolean  "chef_is_me",    :default => false, :null => false
+    t.text     "cuisine",       :default => "",    :null => false
+    t.text     "notes",         :default => "",    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "opening_staff",   :default => false
+    t.boolean  "opening_staff", :default => false
   end
 
   add_index "culinary_jobs", ["profile_id"], :name => "index_profile_restaurants_on_profile_id"
@@ -284,9 +282,9 @@ ActiveRecord::Schema.define(:version => 20101022194902) do
     t.integer  "restaurant_role_id"
     t.boolean  "omniscient"
     t.boolean  "primary",            :default => false
-    t.string   "type"
     t.boolean  "public_profile"
     t.integer  "position"
+    t.string   "type"
   end
 
   add_index "employments", ["employee_id"], :name => "index_employments_on_employee_id"
@@ -595,7 +593,7 @@ ActiveRecord::Schema.define(:version => 20101022194902) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "position"
+    t.integer  "position",   :default => 0
     t.integer  "chapter_id"
   end
 
@@ -620,13 +618,6 @@ ActiveRecord::Schema.define(:version => 20101022194902) do
   end
 
   add_index "profiles", ["user_id"], :name => "index_profiles_on_user_id", :unique => true
-
-  create_table "question_role_categories", :force => true do |t|
-    t.integer  "restaurant_role_id"
-    t.string   "category"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "question_roles", :force => true do |t|
     t.integer  "profile_question_id"
