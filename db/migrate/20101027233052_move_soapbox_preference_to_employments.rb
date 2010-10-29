37c0c451f5f19c2de00c76b5011ca93bfd4f9cb0
Mike Busch <busch.michael@gmail.com>
2010-10-29 15:06:38 -0500

    grr... fixing migration issue in staging

--- db/migrate/20101027233052_move_soapbox_preference_to_employments.rb
+++ db/migrate/20101027233052_move_soapbox_preference_to_employments.rb
@@ -1,6 +1,6 @@
 class MoveSoapboxPreferenceToEmployments < ActiveRecord::Migration
   def self.up
-    add_column :employments, :post_to_soapbox, :boolean, :default => true
+    # add_column :employments, :post_to_soapbox, :boolean, :default => true
     # prefers_post_to_soapbox? is removed, previous default was true. cleanup production data by hand if needed.
     # Employment.all.each do |e|
     #   e.update_attribute(:post_to_soapbox, e.prefers_post_to_soapbox?)
