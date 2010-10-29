6d8771ceaba21a67c5e0252433aac165f716dd79
Mike Busch <busch.michael@gmail.com>
2010-10-29 14:54:47 -0500

    removed reference to non-existent method

--- db/migrate/20101027233052_move_soapbox_preference_to_employments.rb
+++ db/migrate/20101027233052_move_soapbox_preference_to_employments.rb
@@ -1,9 +1,10 @@
 class MoveSoapboxPreferenceToEmployments < ActiveRecord::Migration
   def self.up
     add_column :employments, :post_to_soapbox, :boolean, :default => true
-    Employment.all.each do |e|
-      e.update_attribute(:post_to_soapbox, e.prefers_post_to_soapbox?)
-    end
+    # prefers_post_to_soapbox? is removed, previous default was true. cleanup production data by hand if needed.
+    # Employment.all.each do |e|
+    #   e.update_attribute(:post_to_soapbox, e.prefers_post_to_soapbox?)
+    # end
   end
 
   def self.down
