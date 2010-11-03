2f721e079c7fe25990fa5aab742312268df7a6a8
Mike Busch <busch.michael@gmail.com>
2010-11-03 10:18:56 -0500

    fixed issue of using order and group in the same query, caused by differences in sqlite3 and mysql environments

--- app/models/a_la_minute_answer.rb
+++ app/models/a_la_minute_answer.rb
@@ -23,21 +23,17 @@ class ALaMinuteAnswer < ActiveRecord::Base
   # named_scope :newest, :group => :a_la_minute_question_id, :order => 'created_at desc'
 
   def self.newest_for(obj)
+    ids = []
     if obj.is_a?(Restaurant) || obj.is_a?(User)
-      obj.a_la_minute_answers.all(
-          :group => :a_la_minute_question_id,
-          :order => 'created_at desc')
+      ids = obj.a_la_minute_answers.maximum(:created_at, :group => :a_la_minute_question_id, :select => :id).collect{|k,v|v}
+      obj.a_la_minute_answers.find(ids)
     else
-      obj.a_la_minute_answers.all(
-          :group => 'responder_id, responder_type',
-          :order => 'created_at desc')
+      ids = obj.a_la_minute_answers.maximum(:created_at, :group => 'responder_type, responder_id', :select => :id).collect{|k,v|v}
     end
+    obj.a_la_minute_answers.find(ids)
   end
 
   def self.public_profile_for(responder)
-    responder.a_la_minute_answers.find_all_by_show_as_public(true,
-        :group => :a_la_minute_question_id,
-        :order => "created_at desc",
-        :limit => 3)
+    responder.a_la_minute_answers.find_all_by_show_as_public(true, :group => :a_la_minute_question_id, :order => "created_at desc", :limit => 3)
   end
 end
