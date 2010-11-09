b88e2b08285e2cbbd8ad6ac0b4461d7b8cecc763
Joe Sak <joe@joesak.com>
2010-11-09 17:57:36 -0600

    Connect the top thing to the bottom, as Nick would say

--- public/javascripts/spoonfeed.js
+++ public/javascripts/spoonfeed.js
@@ -110,3 +110,44 @@ var colorboxForm = function(){
   return false;
 };
 
+
+// == Dynamic Updates for Employment Searching
+var	$employmentsList  = $("#employment_list");
+var $employmentInputs = $("#employment_criteria input[type=checkbox]");
+var $loaderImg =        $('<img class="loader" src="/images/ajax-loader.gif" />').hide();
+
+// load the image load indicator, hidden
+$employmentsList.before($loaderImg);
+
+function updateEmploymentsList() {
+	input_string = $employmentInputs.serialize();
+	$loaderImg.show();
+	$employmentsList.hide();
+	$employmentsList.load('/employment_search', input_string, function(responseText, textStatus){
+	  $loaderImg.hide();
+	  $employmentsList.fadeIn(300);
+	});
+	// return true;	
+}
+
+$employmentInputs.change(updateEmploymentsList);
+
+// Directory search
+var	$directoryList  = $("#directory_list");
+var $directoryInputs = $("#directory_search #employment_criteria input[type=checkbox]");
+
+$directoryList.before($loaderImg);
+
+function updateDirectoryList() {
+	input_string = $directoryInputs.serialize();
+	$loaderImg.show();
+	$directoryList.hide();
+	$directoryList.load('/directory/search', input_string, function(responseText, textStatus){
+	  $loaderImg.hide();
+	  $directoryList.fadeIn(300);
+	});
+	// return true;	
+}
+
+$directoryInputs.change(updateDirectoryList);
+
