5acf2db1a385238313506dd8b2566020ba4982b3
Audrey Eschright <spinnerin@gmail.com>
2010-11-10 18:19:48 -0800

    Updating slide and promo migrations to ensure existing content stays visible to soapbox.

--- db/migrate/20101104213542_make_promos_generic.rb
+++ db/migrate/20101104213542_make_promos_generic.rb
@@ -2,6 +2,7 @@ class MakePromosGeneric < ActiveRecord::Migration
   def self.up
     rename_table :soapbox_promos, :promos
     add_column :promos, :type, :string
+    Promo.all(:conditions => { :type => nil }).each { |s| s.update_attribute(:type, "SoapboxPromo") }
   end
 
   def self.down
