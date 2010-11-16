1309221209636a5af8d5ef3315c4113c599bd68a
Audrey Eschright <spinnerin@gmail.com>
2010-11-16 12:20:49 -0800

    Enabling selected tab styling on front burner.

--- app/helpers/front_burner_helper.rb
+++ app/helpers/front_burner_helper.rb
@@ -1,2 +1,12 @@
 module FrontBurnerHelper
+  
+  def selected?(name)
+    case name
+    when "All"
+      return "selected" if params[:all].present?
+    when "New"
+      return "selected" if params[:all].nil?
+    end
+    ""
+  end
 end
