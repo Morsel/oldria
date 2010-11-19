be43fa650436bf12e5fe48519349d9649fed4ecd
Nic Haynes <Nic@Parker.local>
2010-11-19 13:31:46 -0600

    Updated publish-to-frontburner label; updated specialties, cuisines labels

--- public/javascripts/spoonfeed.js
+++ public/javascripts/spoonfeed.js
@@ -20,6 +20,7 @@ $('.apprenticeship').equalHeights();
 
 
 $('#profile-tabs').tabs({
+	cookie: { expires: 1 },
 	panelTemplate: '<section></section>',
 	fx: { duration: 'fast', opacity: 'toggle' }
 });
