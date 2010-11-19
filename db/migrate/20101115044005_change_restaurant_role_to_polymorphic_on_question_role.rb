a76a5abf0d59018a6fc2d8c93eecf1afc2db96d0
Mike Busch <busch.michael@gmail.com>
2010-11-19 12:06:04 -0600

    changed migrations to be more failsafe

--- db/migrate/20101115044005_change_restaurant_role_to_polymorphic_on_question_role.rb
+++ db/migrate/20101115044005_change_restaurant_role_to_polymorphic_on_question_role.rb
@@ -4,11 +4,7 @@ class ChangeRestaurantRoleToPolymorphicOnQuestionRole < ActiveRecord::Migration
     add_column :question_roles, :responder_type, :string
 
     QuestionRole.all.each do |qr|
-      if qr.profile_question.chapter.topic.responder_type == 'restaurant'
-        qr.update_attribute(:responder_type, 'RestaurantFeaturePage')
-      else
-        qr.update_attribute(:responder_type, 'RestaurantRole')
-      end
+      qr.update_attribute(:responder_type, 'RestaurantRole')
     end
   end
 
