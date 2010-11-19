c41b2753219c33b0d9e1460240d13c70c04f8002
Joe Sak <joe@joesak.com>
2010-11-19 17:05:18 -0600

    more fixing of that selected nav logic

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -22,7 +22,7 @@ module LayoutHelper
   end
 
   def active_link_to text, path, options = {}
-    css_class = request.path.split('/').include?(path.split('/')[1]) ? ' selected' : ''
+    css_class = (request.path == path || request.path.split('/').include?(path.split('/')[1])) ? ' selected' : ''
     options[:class] ||= ""
     options[:class] << css_class
     link_to text, path, options
