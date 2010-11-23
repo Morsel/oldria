b1005c14279e5aae22c7c6c5e52345c5916dcbb1
Joe Sak <joe@joesak.com>
2010-11-23 15:50:17 -0600

    Promos & slides wired up, cycle plugin included, HQ layout links linked

--- app/controllers/hq/hq_controller.rb
+++ app/controllers/hq/hq_controller.rb
@@ -2,5 +2,7 @@ class Hq::HqController < ApplicationController
   layout 'hq'
   
   def index
+    @hq_slides = HqSlide.all
+    @hq_promos = HqPromo.all
   end
 end
--- public/javascripts/ria.js
+++ public/javascripts/ria.js
@@ -1,2 +1,14 @@
+$('#jumbotron').cycle({
+	fx: 'scrollHorz',
+	timeout: 8000,
+	easing: 'easeInOutCubic',
+	pager: '#controller',
+	pagerBuilder: jumbotronController
+});
+
 $('#bottom_promos section').equalHeights();
 
+function jumbotronController(idx, elem){
+	idx++;
+	return html+='<a href="#">'+idx+'</a>';
+}
