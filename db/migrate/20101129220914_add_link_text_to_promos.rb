7cd7b277477d9091f2cb52abbc43bfabc281c94c
Joe Sak <joe@joesak.com>
2010-11-29 16:13:50 -0600

    Link text on HQ promos

---
+++ db/migrate/20101129220914_add_link_text_to_promos.rb
@@ -0,0 +1,9 @@
+class AddLinkTextToPromos < ActiveRecord::Migration
+  def self.up
+    add_column :promos, :link_text, :string
+  end
+
+  def self.down
+    remove_column :promos, :link_text
+  end
+end
