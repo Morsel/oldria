921bf5c156d163cb65514a7b7b52d22edca89892
Ed Blake <ed@neotericdesign.com>
2010-12-03 16:44:18 -0600

    additional 'restaurants' edit styling

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,17 +1,17 @@
 module EmployeesHelper
   def restaurant_role_and_link(employment)
     role_name = employment.restaurant_role.try(:name) || "<em>No Role Assigned</em>"
-    delete_link = link_to('<span class="delete">Delete</span>',
+    delete_link = link_to('Delete',
                         restaurant_employee_path(employment.restaurant, employment.employee),
                         :method => :delete,
                         :confirm => 'Are you sure?',
-                        :title => 'Delete')
-    edit_link = link_to('<span class="edit">edit</span>',
+                        :title => 'Delete',
+                        :class => "delete")
+    edit_link = link_to('Edit',
                          edit_restaurant_employee_path(employment.restaurant, employment.employee),
-                         :title => 'Edit')
-    content_tag(:p, :class => 'restaurant_role') do
-      "#{role_name} #{delete_link} #{edit_link}"
-    end
+                         :title => 'Edit',
+                         :class => 'edit')
+    "<p class='restaurant_role'>#{role_name}</p>#{delete_link} #{edit_link}"
   end
 
 
