fefece5675bfb64df715b9ffd5f2d0fff073fc40
Audrey Eschright <spinnerin@gmail.com>
2010-12-06 13:36:42 -0800

    Fixing trend question sort order.

--- app/controllers/front_burner_controller.rb
+++ app/controllers/front_burner_controller.rb
@@ -6,11 +6,11 @@ class FrontBurnerController < ApplicationController
     if params[:all]
       @qotds = current_user.admin_conversations.current
       resto_trends = current_user.grouped_trend_questions.keys
-      @trend_questions = resto_trends.present? ? resto_trends : current_user.solo_discussions.current
+      @trend_questions = (resto_trends.present? ? resto_trends : current_user.solo_discussions.current).sort_by(&:scheduled_at).reverse
     else
       @qotds = current_user.unread_qotds
       resto_trends = current_user.unread_grouped_trend_questions.keys
-      @trend_questions = resto_trends.present? ? resto_trends : current_user.unread_solo_discussions
+      @trend_questions = (resto_trends.present? ? resto_trends : current_user.unread_solo_discussions).sort_by(&:scheduled_at).reverse
     end
   end
 
