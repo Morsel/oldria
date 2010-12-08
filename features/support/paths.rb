8904f0f41b449ade42915066eceab3de08ab31d9
Mike Busch <busch.michael@gmail.com>
2010-12-08 14:18:55 -0600

    fixing cukes

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -131,15 +131,7 @@ module NavigationHelpers
     when /^the admin invitations page$/
       admin_invitations_path
     when /^the restaurant photo upload page for (.+)$/
-      restaurant_photos_path(Restaurant.find_by_name($1))
-    when /^the admin restaurant feature page$/
-      admin_restaurant_features_path
-    when /^the restaurant photo upload page for (.+)$/
-      edit_photos_admin_restaurant_path(Restaurant.find_by_name($1))
-    when /^the admin restaurant feature page$/
-      admin_restaurant_features_path
-    when /^the restaurant photo upload page for (.+)$/
-      edit_photos_restaurant_path(Restaurant.find_by_name($1))
+      bulk_edit_restaurant_photos_path(Restaurant.find_by_name($1))
     when /^the admin restaurant feature page$/
       admin_restaurant_features_path
     when /^the admin a la minute questions page$/
