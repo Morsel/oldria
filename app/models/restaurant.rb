e7f702411bfb0921c2d4c7c67ad55dc4e013f032
Audrey Eschright <spinnerin@gmail.com>
2010-12-15 12:14:06 -0800

    Removing restaurant hours validation.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -85,7 +85,7 @@ class Restaurant < ActiveRecord::Base
   accepts_nested_attributes_for :logo
 
   validates_presence_of :name, :street1, :city, :state, :zip, :phone_number,
-      :metropolitan_area, :website, :media_contact, :hours, :cuisine, :opening_date, :manager
+      :metropolitan_area, :website, :media_contact, :cuisine, :opening_date, :manager
 
   validates_format_of :website, :with => URI::regexp(%w(http https)),
       :message => "needs to be a valid URL that starts with http://"
