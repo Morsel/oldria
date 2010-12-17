a73ec62c804359ff14e03df3d0f761194d980680
Dmitriy Zaporozhets <dzaporozhets@sphereconsultinginc.com>
2010-12-17 17:16:30 +0200

    [#7017029] fixed logic to match new ProfileAnswer association

--- app/helpers/profile_answers_helper.rb
+++ app/helpers/profile_answers_helper.rb
@@ -1,11 +1,10 @@
 module ProfileAnswersHelper
-  def url_to_responder_question(answer)
-    responder = answer.responder
+  def url_to_responder_question(responder, chapter_id)
     case true 
     when responder.is_a?(User)
-      then soapbox_user_questions_url(responder, :chapter_id => answer.profile_question.chapter)
+      then soapbox_user_questions_url(responder, :chapter_id => chapter_id)
     when responder.is_a?(Restaurant)
-      then soapbox_restaurant_questions_url(responder, :chapter_id => answer.profile_question.chapter)
+      then soapbox_restaurant_questions_url(responder, :chapter_id => chapter_id)
     else
       raise "Unsupported responder type for sharing the question url"
     end
