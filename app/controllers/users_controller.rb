7d6be6a841f34848dae60e8881171ee8ff953711
Audrey Eschright <spinnerin@gmail.com>
2010-12-21 13:27:59 -0800

    Updating media confirmation to redirect user to mediafeed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
       @user_session = UserSession.new(@user)
       if @user_session.save
         @message = "Welcome aboard! Your account has been confirmed."
-        redirect_to root_path if @user.media?
+        redirect_to mediafeed_root_path if @user.media?
       else
         @message = "Could not log you in. Please contact us for assistance."
       end
