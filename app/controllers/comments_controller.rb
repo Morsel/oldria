2051da2d48113994d9499044f38e7dc59f37dc49
Audrey Eschright <spinnerin@gmail.com>
2010-12-23 14:11:50 -0800

    Updating comment create messages: archive message happens only for front burner content when there isn't another trend question discussion in the group that will remain unread.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,15 +6,31 @@ class CommentsController < ApplicationController
     @comment = @parent.comments.build(params[:comment])
     @comment.user_id ||= current_user.id
     @is_mediafeed = params[:mediafeed]
+    success_and_archive = "Thanks: your answer has been saved. The question has been archived and can be found under the \"all\" tab."
+    success = "Thanks: your answer has been saved."
+
     if @comment.save
       if front_burner_content
         @parent.read_by!(@comment.user)
-        flash[:notice] = "Thanks: your answer has been saved. The question has been archived and can be found under the \"all\" tab."
+
+        # if the parent is attached to a trend question, show archive message only when it's the first discussion for the user
+        # why the first discussion? because we only check the first item's read/unread status in the inbox when we group these
+        if @parent.is_a?(AdminDiscussion)
+          current_user.grouped_admin_discussions[@parent.discussionable].first == @parent ? 
+              flash[:notice] = success_and_archive :
+              flash[:notice] = success
+        else
+          flash[:notice] = success_and_archive
+        end
       else
-        flash[:notice] = "Thanks: your answer has been saved."
+        flash[:notice] = success
       end
-      
-      redirect_to mediafeed? ? mediafeed_discussion_path(@parent.media_request, @parent.class.name.pluralize.underscore.downcase, @parent) : @parent
+
+      if mediafeed?
+        redirect_to mediafeed_discussion_path(@parent.media_request, @parent.class.name.pluralize.underscore.downcase, @parent)
+      else
+        redirect_to @parent
+      end
     else
       flash[:error] = "Your comment couldn't be saved."
       redirect_to :back
