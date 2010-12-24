80b8e00bf270bfc2a7bf5b7e68c9ab23b6fb451e
Mykhaylo Sorochan <msorochan@sphereconsultinginc.com>
2010-12-24 15:32:45 +0200

    [#7544027]

--- app/controllers/a_la_minute_answers_controller.rb
+++ app/controllers/a_la_minute_answers_controller.rb
@@ -19,30 +19,23 @@ class ALaMinuteAnswersController < ApplicationController
   def bulk_update
     @restaurant = Restaurant.find(params[:restaurant_id])
 
-    if show_as_public_count_valid?
-      params[:a_la_minute_questions].each do |id, attributes|
-        question = ALaMinuteQuestion.find(id)
-        old_answer = attributes.delete(:old_answer)
+    params[:a_la_minute_questions].each do |id, attributes|
+      question = ALaMinuteQuestion.find(id)
+      old_answer = attributes.delete(:old_answer)
 
-        # create a new answer if the answer has changed
-        unless attributes[:answer] == old_answer
-          @restaurant.a_la_minute_answers.create(
-              attributes.merge(:a_la_minute_question_id => id))
-        end
-
-        # update all answers for the question to reflect show_as_public state
-        question.answers_for(@restaurant).each do |answer|
-          answer.update_attributes(:show_as_public => attributes[:show_as_public].present?)
-        end
-        flash[:success] = "Your changes have been saved."
+      # create a new answer if the answer has changed
+      unless attributes[:answer] == old_answer
+        @restaurant.a_la_minute_answers.
+          create(attributes.merge(:a_la_minute_question_id => id))
       end
-      redirect_to :action => :bulk_edit
-    else
-      flash.now[:error] = "Sorry, only 3 answers can be displayed on your Soapbox Profile page."
-      @questions = ALaMinuteQuestion.restaurants
-      render :bulk_edit
-    end
 
+      # update all answers for the question to reflect show_as_public state
+      question.answers_for(@restaurant).each do |answer|
+        answer.update_attributes(:show_as_public => attributes[:show_as_public].present?)
+      end
+      flash[:success] = "Your changes have been saved."
+    end
+    redirect_to :action => :bulk_edit
   end
 
   # sent from the eip javascript function
