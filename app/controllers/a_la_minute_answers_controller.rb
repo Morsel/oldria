5e840e4eed046729e74e72f0ec97720f5c8154c7
Mykhaylo Sorochan <msorochan@sphereconsultinginc.com>
2010-12-27 17:37:57 +0200

    [#7829333] remove edit_in_place

--- app/controllers/a_la_minute_answers_controller.rb
+++ app/controllers/a_la_minute_answers_controller.rb
@@ -36,25 +36,6 @@ class ALaMinuteAnswersController < ApplicationController
     redirect_to :action => :bulk_edit
   end
 
-  # sent from the eip javascript function
-  # see http://josephscott.org/code/javascript/jquery-edit-in-place/ for
-  # parameters
-  def edit_in_place
-    id = params[:id].split("_")[-1]
-    @question = ALaMinuteQuestion.find_by_id(id)
-    @restaurant = Restaurant.find_by_id(params[:data].split("=").last)
-    if @restaurant && @question
-      @answer = ALaMinuteAnswer.create(:answer => params[:new_value].strip,
-          :responder => @restaurant, :a_la_minute_question => @question)
-    end
-    if @restaurant && @question && @answer.valid?
-      render :text => {:is_error => false, :error_text => nil, :html => @answer.answer}.to_json
-    else
-      render :text => {:is_error => true, :error_text => "Error updating answer",
-          :html => params[:orig_value]}.to_json
-    end
-  end
-
   private
 
   def require_restaurant_employee
