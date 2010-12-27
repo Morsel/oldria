e0023c73e3a4b1246df0e408ec7b347090f54102
Dmitriy Zaporozhets <dzaporozhets@sphereconsultinginc.com>
2010-12-27 15:25:19 +0200

    [7505445] restaurant helper, spec test for helper. Some views updated

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,13 @@
 module RestaurantsHelper
+  def restaurant_link(restaurant, options = nil)
+    if (params[:controller].match(/soapbox/) || params[:controller].match(/mediafeed/))
+      if restaurant.premium_account?
+        link_to(h(restaurant.name), soapbox_restaurant_path(restaurant), options) 
+      else
+        h(restaurant.name)
+      end
+    else
+      link_to(h(restaurant.name), restaurant_path(restaurant), options)
+    end
+  end
 end
