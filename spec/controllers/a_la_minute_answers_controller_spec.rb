7198c68d990b2cd23d2416ac7a53a83370549d6b
Mykhaylo Sorochan <msorochan@sphereconsultinginc.com>
2010-12-30 14:52:48 +0200

    fix ALM specs and small bug in controller

--- spec/controllers/a_la_minute_answers_controller_spec.rb
+++ spec/controllers/a_la_minute_answers_controller_spec.rb
@@ -5,13 +5,20 @@ describe ALaMinuteAnswersController do
 
   let(:restaurant) { Factory(:restaurant, :id => 1) }
   let(:question) { Factory(:a_la_minute_question, :id => 1) }
+  let(:current_user) { Factory(:user) }
+
+  before(:each) do
+    Factory(:employment, :restaurant => restaurant, :employee => current_user)
+    controller.stubs(:current_user).returns current_user
+  end
 
   describe "PUT bulk_update" do
     describe "successful update" do
       before(:each) do
-        answer = ALaMinuteAnswer.create(:answer => "old answer", 
+        answer = ALaMinuteAnswer.create(:answer => "old answer",
             :a_la_minute_question => question, :responder => restaurant,
             :created_at => 1.day.ago, :show_as_public => false)
+
         put :bulk_update, "restaurant_id" => restaurant.id,
           "a_la_minute_questions" => {
             question.id.to_s => {
@@ -20,16 +27,16 @@ describe ALaMinuteAnswersController do
             }
           }
       end
-      
+
       specify { question.should have(2).a_la_minute_answers}
       specify { question.answer_for(restaurant).answer.should == "new answer" }
       specify { question.answer_for(restaurant).show_as_public.should be_false }
       specify { response.should redirect_to(bulk_edit_restaurant_a_la_minute_answers_path(restaurant)) }
     end
-    
-    describe "update if answer doesn't change" do 
+
+    describe "update if answer doesn't change" do
       before(:each) do
-        answer = ALaMinuteAnswer.create(:answer => "old answer", 
+        answer = ALaMinuteAnswer.create(:answer => "old answer",
             :a_la_minute_question => question, :responder => restaurant)
         put :bulk_update, "restaurant_id" => restaurant.id,
           "a_la_minute_questions" => {
@@ -40,47 +47,11 @@ describe ALaMinuteAnswersController do
             }
           }
       end
-      
+
       specify { question.should have(1).a_la_minute_answers}
       specify { question.answer_for(restaurant).show_as_public.should be_true }
     end
-    
-    
-  end
-
-  describe "POST edit_in_place" do
-
-    describe "successful update" do
-      before(:each) do
-        post :edit_in_place, "new_value" => "What's new",
-            "data" => "restaurant_id=#{restaurant.id}",
-            "id" => dom_id(question), "orig_value"=> "Pretty"
-        @answer = assigns[:answer]
-      end
-
-      it "should return the new text on successful update" do
-        ActiveSupport::JSON.decode(response.body) == {"is_error" => false,
-            "error_text" => nil, "html" => "What's new"}
-      end
-
-      specify { @answer.a_la_minute_question.should == question }
-      specify { @answer.responder.should == restaurant }
-      specify { @answer.answer.should == "What's new" }
-    end
-
-    describe "unsuccessful update" do
-      before(:each) do
-        post :edit_in_place, "new_value" => "What's new",
-            "data" => "restaurant_id=#{restaurant.id + 1}",
-            "id" => dom_id(question), "orig_value"=> "Pretty"
-        @answer = assigns[:answer]
-      end
-
-      it "should return the new text on successful update" do
-        ActiveSupport::JSON.decode(response.body).should == {"is_error" => true,
-            "error_text" => "Error updating answer", "html" => "Pretty"}
-      end
-    end
 
   end
+
 end
