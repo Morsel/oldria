2c2ba3e9a97d81543106b4530bfdc84016b3d16c
Audrey Eschright <spinnerin@gmail.com>
2011-01-03 16:14:15 -0800

    Cleanup (old comment no longer applies).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,6 @@ class ApplicationController < ActionController::Base
 
   # before_filter :preload_resources
   
-  # Disabled until we restore btl_game in the sidebar
   before_filter :load_random_btl_question
 
   helper_method :current_user
