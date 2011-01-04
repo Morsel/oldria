2e9f16735c2511503d411da33c2468dc542112cc
Audrey Eschright <spinnerin@gmail.com>
2011-01-04 11:55:02 -0800

    Adding more comments for future refactoring.

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,4 +1,5 @@
 module MailerHelper
+  # FIXME - the word is spelled indefinite, and the message text could probably be rewritten to eliminate this helper
   def indefenite_article(word)
     (word.to_s =~ /^[aeio]+/i) ? "an" : "a"
   end
