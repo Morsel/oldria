424335a3460dbedb369487355e4a0522743d29eb
Audrey Eschright <spinnerin@gmail.com>
2011-02-28 09:54:13 -0800

    Fixing specs.

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -9,36 +9,37 @@ describe ProfilesController do
     fake_normal_user
     profile = Factory(:profile, :user => @user)
     @user.stubs(:profile).returns(profile)
+    User.stubs(:find).returns(@user)
   end
 
   it "edit action should render edit template" do
-    get :edit
+    get :edit, :user_id => @user.id
     response.should render_template(:edit)
   end
 
   it "update action should render edit template when model is invalid" do
     Profile.any_instance.stubs(:valid?).returns(false)
-    put :update
+    put :update, :user_id => @user.id
     response.should render_template(:edit)
   end
 
   it "update action should redirect when model is valid" do
     Profile.any_instance.stubs(:valid?).returns(true)
-    put :update
+    put :update, :user_id => @user.id
     response.should redirect_to( profile_path(@user.username) )
   end
   
   it "should set the primary employment" do
     employment = Factory(:employment, :employee => @user)
     employment2 = Factory(:employment, :employee => @user)
-    put :update, :profile => { :primary_employment => [employment2.id.to_s] }
+    put :update, :user_id => @user.id, :profile => { :primary_employment => [employment2.id.to_s] }
     @user.primary_employment.should == employment2
   end
   
   it "should clear an old primary employment" do
     employment = Factory(:employment, :employee => @user)
     employment2 = Factory(:employment, :employee => @user, :primary => true)
-    put :update, :profile => { :primary_employment => [employment.id.to_s] }
+    put :update, :user_id => @user.id, :profile => { :primary_employment => [employment.id.to_s] }
     @user.primary_employment.should == employment
     Employment.find(employment2.id).primary.should == false
   end
@@ -49,12 +50,13 @@ end
 
     before(:each) do
       fake_normal_user
+      User.stubs(:find).returns(@user)
     end
 
     it "should set the primary employment when creating a new profile" do
       employment = Factory(:employment, :employee => @user)
       employment2 = Factory(:employment, :employee => @user)
-      post :create, :profile => { :primary_employment => [employment.id.to_s] }
+      post :create, :user_id => @user.id, :profile => { :primary_employment => [employment.id.to_s] }
       @user.primary_employment.should == employment
     end
 
