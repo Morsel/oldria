783fb453132d03152204898490d81c7ad2ecab68
Audrey Eschright <spinnerin@gmail.com>
2011-03-22 16:57:25 -0700

    Updating questions controller to remove non-user conditions.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,22 +3,19 @@ class QuestionsController < ApplicationController
 
   before_filter :require_user_unless_soapbox
   before_filter :get_subject, :except => [:show]
-  before_filter :get_profile, :only => :topics
 
   def index
     @chapter = Chapter.find(params[:chapter_id])
 
-    is_self = can_edit_profile_questions(@subject)
+    is_self = can? :manage, @subject
     @previous = @chapter.previous_for_subject(@subject, is_self)
     @next = @chapter.next_for_subject(@subject, is_self)
     @previous_topic = @chapter.topic.previous_for_subject(@subject, is_self)
     @next_topic = @chapter.topic.next_for_subject(@subject, is_self)
 
     @questions = is_self ?
-        @chapter.profile_questions.for_subject(@subject) :
-        @subject.is_a?(RestaurantFeaturePage) ?
-            ProfileQuestion.answered_for_page(@subject, @restaurant).for_chapter(params[:chapter_id]) :
-            ProfileQuestion.answered_for_subject(@subject).for_chapter(params[:chapter_id])
+      @chapter.profile_questions.for_subject(@subject) :
+      ProfileQuestion.answered_for_subject(@subject).for_chapter(params[:chapter_id])
   end
 
   def show
@@ -27,20 +24,16 @@ class QuestionsController < ApplicationController
   end
 
   def topics
-    if can_edit_profile_questions(@subject)
+    if can? :manage, @subject
       @topics = Topic.for_subject(@subject)
       chapters = @topics.collect do |topic|
         topic.chapters.for_subject(@subject).all(:limit => 3)
       end
       @chapters_by_topic = chapters.flatten.group_by(&:topic)
     else
-      @topics = @subject.is_a?(RestaurantFeaturePage) ?
-                  Topic.answered_for_page(@subject, @restaurant) :
-                  Topic.answered_for_subject(@subject)
+      @topics = Topic.answered_for_subject(@subject)
       chapters = @topics.collect do |topic|
-        @subject.is_a?(RestaurantFeaturePage) ?
-          topic.chapters.answered_for_page(@subject, @restaurant).all(:limit => 3) :
-          topic.chapters.answered_for_subject(@subject).all(:limit => 3)
+        topic.chapters.answered_for_subject(@subject).all(:limit => 3)
       end
       @chapters_by_topic = chapters.flatten.group_by(&:topic)
     end
@@ -48,14 +41,14 @@ class QuestionsController < ApplicationController
 
   def chapters
     @topic = Topic.find(params[:topic_id])
-    is_self = can_edit_profile_questions(@subject)
+    is_self = can? :manage, @subject
     @previous = @topic.previous_for_subject(@subject, is_self)
     @next = @topic.next_for_subject(@subject, is_self)
 
-    @questions_by_chapter = @subject.profile_questions.
-        all(:conditions => { :chapter_id => @topic.chapters.map(&:id) }, :joins => :chapter,
-        :order => "chapters.position, chapters.id").
-        group_by(&:chapter)
+    @questions_by_chapter = @subject.profile_questions.all(:conditions => { :chapter_id => @topic.chapters.map(&:id) }, 
+                                                                            :joins => :chapter,
+                                                                            :order => "chapters.position, chapters.id").
+                                                                            group_by(&:chapter)
   end
 
   def refresh
@@ -66,21 +59,7 @@ class QuestionsController < ApplicationController
   protected
 
   def get_subject
-    if params[:user_id]
-      @subject = User.find(params[:user_id])
-    elsif params[:feature_page_id] || params[:feature_id]
-      id = params[:feature_page_id] || params[:feature_id]
-      @subject = RestaurantFeaturePage.find(id)
-      @restaurant = Restaurant.find(params[:restaurant_id])
-    else
-      @subject = Restaurant.find(params[:restaurant_id])
-    end
-  end
-
-  def get_profile
-    if @subject.is_a? User
-      @profile = @subject.profile || @subject.build_profile
-    end
+    @subject = User.find(params[:user_id])
   end
 
 end
