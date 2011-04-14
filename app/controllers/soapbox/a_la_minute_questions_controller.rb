308284537cef83329a08dfcd54ebe4e656e02286
Audrey Eschright <spinnerin@gmail.com>
2011-04-14 14:50:58 -0700

    ALM SB updates (styling, restaurant linking, number of items displayed, share buttons).

--- app/controllers/soapbox/a_la_minute_questions_controller.rb
+++ app/controllers/soapbox/a_la_minute_questions_controller.rb
@@ -3,18 +3,17 @@ class Soapbox::ALaMinuteQuestionsController < ApplicationController
   def index
     @questions = ALaMinuteQuestion.all(:include => "a_la_minute_answers",
                                        :order => "a_la_minute_answers.created_at DESC",
-                                       :limit => 5)
+                                       :limit => 10)
     @sidebar_questions = ALaMinuteQuestion.all(:include => "a_la_minute_answers",
                                                :order => "a_la_minute_answers.created_at DESC",
-                                               :limit => 10, :offset => 5)
+                                               :offset => 10)
   end
 
   def show
     @question = ALaMinuteQuestion.find(params[:id])
     @answers = ALaMinuteAnswer.newest_for(@question)
     @sidebar_questions = ALaMinuteQuestion.all(:include => "a_la_minute_answers",
-                                               :order => "a_la_minute_answers.created_at DESC",
-                                               :limit => 10)
+                                               :order => "a_la_minute_answers.created_at DESC")
   end
 
 end
