424ee4169ca71397faf9cde29170750ed1eaf02c
Audrey Eschright <spinnerin@gmail.com>
2011-04-28 15:57:16 -0700

    Updating ALM sidebar.

--- app/controllers/soapbox/a_la_minute_questions_controller.rb
+++ app/controllers/soapbox/a_la_minute_questions_controller.rb
@@ -4,16 +4,13 @@ class Soapbox::ALaMinuteQuestionsController < ApplicationController
     @questions = ALaMinuteQuestion.all(:include => "a_la_minute_answers",
                                        :order => "a_la_minute_answers.created_at DESC",
                                        :limit => 10)
-    @sidebar_questions = ALaMinuteQuestion.all(:include => "a_la_minute_answers",
-                                               :order => "a_la_minute_answers.created_at DESC",
-                                               :offset => 10)
+    @sidebar_questions = ALaMinuteQuestion.all(:order => "question")
   end
 
   def show
     @question = ALaMinuteQuestion.find(params[:id])
     @answers = ALaMinuteAnswer.from_premium_responders.newest_for(@question)
-    @sidebar_questions = ALaMinuteQuestion.all(:include => "a_la_minute_answers",
-                                               :order => "a_la_minute_answers.created_at DESC")
+    @sidebar_questions = ALaMinuteQuestion.all(:order => "question")
   end
 
 end
