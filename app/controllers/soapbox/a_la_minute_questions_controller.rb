d205f1e4e9dd0bacb89d59142401be8f7608d4b2
Audrey Eschright <spinnerin@gmail.com>
2011-05-02 12:21:17 -0700

    Fixing ALM index view to remove non-public answers.

--- app/controllers/soapbox/a_la_minute_questions_controller.rb
+++ app/controllers/soapbox/a_la_minute_questions_controller.rb
@@ -3,13 +3,14 @@ class Soapbox::ALaMinuteQuestionsController < ApplicationController
   def index
     @questions = ALaMinuteQuestion.all(:include => "a_la_minute_answers",
                                        :order => "a_la_minute_answers.created_at DESC",
+                                       :conditions => ["`a_la_minute_answers`.show_as_public = ?", true],
                                        :limit => 10)
     @sidebar_questions = ALaMinuteQuestion.all(:order => "question")
   end
 
   def show
     @question = ALaMinuteQuestion.find(params[:id])
-    @answers = ALaMinuteAnswer.from_premium_responders.newest_for(@question)
+    @answers = ALaMinuteAnswer.from_premium_responders.show_public.newest_for(@question)
     @sidebar_questions = ALaMinuteQuestion.all(:order => "question")
   end
 
