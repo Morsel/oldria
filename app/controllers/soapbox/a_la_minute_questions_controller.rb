7ad2efdc7e78665b908c286d3186ef990c5c8e16
Audrey Eschright <spinnerin@gmail.com>
2011-05-02 14:46:20 -0700

    Revert "Tweaking joins."
    This reverts commit cc9f44830990321ab87d3a6cc8b4fba3a752a3cd.

--- app/controllers/soapbox/a_la_minute_questions_controller.rb
+++ app/controllers/soapbox/a_la_minute_questions_controller.rb
@@ -1,8 +1,7 @@
 class Soapbox::ALaMinuteQuestionsController < ApplicationController
 
   def index
-    @questions = ALaMinuteQuestion.all(:joins => :a_la_minute_answers,
-                                       :select => "DISTINCT `a_la_minute_questions`.*",
+    @questions = ALaMinuteQuestion.all(:include => "a_la_minute_answers",
                                        :order => "a_la_minute_answers.created_at DESC",
                                        :conditions => ["`a_la_minute_answers`.show_as_public = ?", true])[0...10]
     @sidebar_questions = ALaMinuteQuestion.all(:order => "question")
