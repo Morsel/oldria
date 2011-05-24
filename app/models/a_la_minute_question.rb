17aaf97b417a6df0fef91a0f15b5537c0f644207
Audrey Eschright <spinnerin@gmail.com>
2011-05-24 10:04:00 -0700

    Cleaning up ALM query for SB homepage.

--- app/models/a_la_minute_question.rb
+++ app/models/a_la_minute_question.rb
@@ -46,10 +46,12 @@ class ALaMinuteQuestion < ActiveRecord::Base
                    ON `subscriptions`.subscriber_id = `a_la_minute_answers`.responder_id
                    AND `subscriptions`.subscriber_type = `a_la_minute_answers`.responder_type',
         :order => "a_la_minute_answers.created_at DESC",
+        :group => "a_la_minute_answers.id",
+        :limit => count,
         :conditions => ["`a_la_minute_answers`.show_as_public = ?
                          AND subscriptions.id IS NOT NULL
                          AND (subscriptions.end_date IS NULL OR subscriptions.end_date >= ?)",
-                         true, Date.today]).uniq[0...count]
+                         true, Date.today])
   end
 
 end
