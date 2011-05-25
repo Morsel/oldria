1956450d5aedfcb973ff60e64257d6360421d126
Audrey Eschright <spinnerin@gmail.com>
2011-05-25 09:47:45 -0700

    Caching front burner content on home page.

--- app/controllers/soapbox/soapbox_controller.rb
+++ app/controllers/soapbox/soapbox_controller.rb
@@ -3,7 +3,7 @@ class Soapbox::SoapboxController < ApplicationController
   def index
     @home = true
 
-    # Right sidebar content
+    # Left sidebar content
     @alm_links = ALaMinuteQuestion.answered.all(:order => "topic, question").uniq
     @newsfeed_links = PromotionType.used_by_promotions.all(:order => :name).uniq
     @btl_links = Topic.user_topics.without_travel.all(:order => "title")
