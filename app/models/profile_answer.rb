e87fd075a869a33be877db2bbb568ab8a554e3ca
Audrey Eschright <spinnerin@gmail.com>
2011-08-19 13:41:18 -0700

    Limiting profile answer length to 1500 chars.

--- app/models/profile_answer.rb
+++ app/models/profile_answer.rb
@@ -18,6 +18,7 @@ class ProfileAnswer < ActiveRecord::Base
 
   validates_presence_of :answer, :profile_question_id, :user_id
   validates_uniqueness_of :profile_question_id, :scope => :user_id
+  validates_length_of :answer, :maximum => 1500
 
   attr_accessor :post_to_facebook, :share_url
   after_save    :post_to_facebook
