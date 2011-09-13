7b45e5657a0517c0625738d40d09aad5b40cfe34
Audrey Eschright <spinnerin@gmail.com>
2011-09-13 13:01:05 -0700

    Removing RIA Unplugged feed content from HQ pages.

--- app/controllers/hq_pages_controller.rb
+++ app/controllers/hq_pages_controller.rb
@@ -3,8 +3,6 @@ class HqPagesController < Hq::HqController
   
   def show
     @page = HqPage.find(params[:id])
-    @feed = Feedzirra::Feed.fetch_and_parse('http://feeds.feedburner.com/RIAUnplugged')
-    @feed.entries.sort! {|x,y| y.published <=> x.published }
   end
   
 end
