9614e2840e1d8f9239a5e876d74a95bc530756d6
Audrey Eschright <spinnerin@gmail.com>
2011-09-20 12:12:54 -0700

    Cleaning up employment searches with post_to_soapbox criteria.

---
+++ db/migrate/20110920181709_remove_soapbox_flag_from_employment_searches.rb
@@ -0,0 +1,14 @@
+class RemoveSoapboxFlagFromEmploymentSearches < ActiveRecord::Migration
+  def self.up
+    EmploymentSearch.all.each do |search|
+      search.conditions.delete(:post_to_soapbox)
+      unless search.save
+        puts "Can't save EmploymentSearch #{search.id}"
+      end
+    end
+  end
+
+  def self.down
+    raise "Can't be reversed"
+  end
+end
