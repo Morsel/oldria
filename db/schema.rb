78cc6bbad0acec379444418f3c50e6fe29159fb4
Audrey Eschright <spinnerin@gmail.com>
2011-09-26 14:45:07 -0700

    Updating nav tabs for logged-in media.

--- db/schema.rb
+++ db/schema.rb
@@ -213,8 +213,8 @@ ActiveRecord::Schema.define(:version => 20110920181709) do
     t.date     "date_ended"
     t.string   "chef_name",       :default => "",    :null => false
     t.boolean  "chef_is_me",      :default => false, :null => false
-    t.text     "cuisine",                            :null => false
-    t.text     "notes",                              :null => false
+    t.text     "cuisine",         :default => "",    :null => false
+    t.text     "notes",           :default => "",    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "opening_staff",   :default => false
@@ -297,9 +297,9 @@ ActiveRecord::Schema.define(:version => 20110920181709) do
     t.integer  "restaurant_role_id"
     t.boolean  "omniscient"
     t.boolean  "primary",              :default => false
+    t.string   "type"
     t.boolean  "public_profile",       :default => true
     t.integer  "position"
-    t.string   "type"
     t.string   "solo_restaurant_name"
   end
 
@@ -339,6 +339,8 @@ ActiveRecord::Schema.define(:version => 20110920181709) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "feed_categories", ["id"], :name => "index_feed_categories_on_id", :unique => true
 
   create_table "feed_entries", :force => true do |t|
     t.string   "title"
@@ -610,8 +612,8 @@ ActiveRecord::Schema.define(:version => 20110920181709) do
     t.string   "country",            :default => "", :null => false
     t.date     "date_started",                       :null => false
     t.date     "date_ended"
-    t.text     "responsibilities",                   :null => false
-    t.text     "reason_for_leaving",                 :null => false
+    t.text     "responsibilities",   :default => "", :null => false
+    t.text     "reason_for_leaving", :default => "", :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -667,7 +669,7 @@ ActiveRecord::Schema.define(:version => 20110920181709) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "position",          :default => 0
+    t.integer  "position"
     t.integer  "chapter_id"
     t.text     "roles_description"
   end
@@ -687,7 +689,7 @@ ActiveRecord::Schema.define(:version => 20110920181709) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "headline",              :default => ""
-    t.text     "summary"
+    t.text     "summary",               :default => ""
     t.string   "hometown"
     t.string   "current_residence"
     t.integer  "metropolitan_area_id"
