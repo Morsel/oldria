5a1d048f3de72c1330bcdd883c8aaa89f20629a8
Audrey Eschright <spinnerin@gmail.com>
2011-10-26 10:28:45 -0700

    Updating menu item image sizes.

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -20,7 +20,7 @@ class MenuItem < ActiveRecord::Base
   has_many :otm_keywords, :through => :menu_item_keywords
 
   has_attached_file :photo,
-                    :styles => { :large => "1966x2400>", :thumb => "120x160>" }
+                    :styles => { :full => "1966x2400>", :large => "360x480>", :medium => "240x320>", :thumb => "120x160>" }
 
   validates_attachment_content_type :photo,
       :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "image/pjpeg", "image/x-png"],
