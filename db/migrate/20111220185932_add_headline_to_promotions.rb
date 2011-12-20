7f0603917008514a111fd12f951dfd41b71f424f
Audrey Eschright <spinnerin@gmail.com>
2011-12-20 12:40:19 -0800

    Adding headlines to promotions.

---
+++ db/migrate/20111220185932_add_headline_to_promotions.rb
@@ -0,0 +1,9 @@
+class AddHeadlineToPromotions < ActiveRecord::Migration
+  def self.up
+    add_column :promotions, :headline, :string
+  end
+
+  def self.down
+    remove_column :promotions, :headline
+  end
+end
