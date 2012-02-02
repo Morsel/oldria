7d21a942563e7aaa7ca9261808bf560c0f83befa
Audrey Eschright <spinnerin@gmail.com>
2012-02-01 17:08:10 -0800

    Fixing ALM updates to not update extra records and spam the site activity log.

--- spec/controllers/a_la_minute_answers_controller_spec.rb
+++ spec/controllers/a_la_minute_answers_controller_spec.rb
@@ -4,8 +4,8 @@ describe ALaMinuteAnswersController do
   include ActionView::Helpers::RecordIdentificationHelper
 
   let(:restaurant) { Factory(:restaurant, :id => 1) }
-  let(:question) { Factory(:a_la_minute_question, :id => 1) }
   let(:current_user) { Factory(:user) }
+  let(:question) { Factory(:a_la_minute_question) }
 
   before(:each) do
     Factory(:employment, :restaurant => restaurant, :employee => current_user)
@@ -15,40 +15,38 @@ describe ALaMinuteAnswersController do
   describe "PUT bulk_update" do
     describe "successful update" do
       before(:each) do
-        answer = ALaMinuteAnswer.create(:answer => "old answer",
-            :a_la_minute_question => question, :responder => restaurant,
-            :created_at => 1.day.ago, :show_as_public => false)
+        previous_answer = Factory(:a_la_minute_answer, :responder => restaurant, :a_la_minute_question => question)
 
         put :bulk_update, "restaurant_id" => restaurant.id,
           "a_la_minute_questions" => {
             question.id.to_s => {
               "answer" => "new answer",
-              "old_answer" => "old answer"
+              "answer_id" => previous_answer.id
             }
           }
       end
 
-      specify { question.should have(2).a_la_minute_answers}
-      specify { question.answer_for(restaurant).answer.should == "new answer" }
+      specify { question.should have(2).a_la_minute_answers }
+      specify { question.reload.answer_for(restaurant).answer.should == "new answer" }
       specify { question.answer_for(restaurant).show_as_public.should be_false }
       specify { response.should redirect_to(bulk_edit_restaurant_a_la_minute_answers_path(restaurant)) }
     end
 
     describe "update if answer doesn't change" do
       before(:each) do
-        answer = ALaMinuteAnswer.create(:answer => "old answer",
-            :a_la_minute_question => question, :responder => restaurant)
+        previous_answer = Factory(:a_la_minute_answer, :responder => restaurant, :a_la_minute_question => question)
+
         put :bulk_update, "restaurant_id" => restaurant.id,
           "a_la_minute_questions" => {
             question.id.to_s => {
-              "answer" => "old answer",
-              "old_answer" => "old answer",
+              "answer" => previous_answer.answer,
+              "answer_id" => previous_answer.id,
               "show_as_public" => "1"
             }
           }
       end
 
-      specify { question.should have(1).a_la_minute_answers}
+      specify { question.should have(1).a_la_minute_answers }
       specify { question.answer_for(restaurant).show_as_public.should be_true }
     end
 
