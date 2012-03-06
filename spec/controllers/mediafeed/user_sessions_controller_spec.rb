8d79cba65c0692d74b543ce8e268fa449d6c9aae
Audrey Eschright <spinnerin@gmail.com>
2012-03-06 11:04:01 -0800

    Fixing mediafeed login redirect.

---
+++ spec/controllers/mediafeed/user_sessions_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Mediafeed::UserSessionsController do
+
+  describe "GET 'new'" do
+    it "should redirect" do
+      get 'new'
+      response.should be_redirect
+    end
+  end
+end
