88e88791883894640948d9c4b8ecaad5ff5f0591
Mike Mangino <mmangino@elevatedrails.com>
2012-03-07 09:04:23 -0500

    Fix problem with go link on stories

--- public/soapbox/_js/site.js
+++ public/soapbox/_js/site.js
@@ -92,7 +92,7 @@ function displayInfo(currSlideElement, nextSlideElement, options, forwardFlag){
 	caption = $(nextSlideElement).attr('data-caption');
 	url = $(nextSlideElement).attr('data-url');
 	credit = 'Photo Credit: ' + $(nextSlideElement).attr('data-credit');
-	link = url.length > 0 ? '&nbsp;<a href=' + url + '>go&nbsp;&#187;</a>' : '';
+	link = url.length > 0 ? '&nbsp;<a href="' + url + '">go&nbsp;&#187;</a>' : '';
 	$('#cycle-title').html(title);
 	$('#cycle-footer').html(credit);
 	$('#cycle-caption').html(caption + link);
