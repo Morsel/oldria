9b2d9dd49079256657c9ca3c5ce52ed74ada5dd0
Audrey Eschright <spinnerin@gmail.com>
2012-03-29 13:48:27 -0700

    Fixing Spoonfeed OTM keyword links.

--- app/controllers/spoonfeed/menu_items_controller.rb
+++ app/controllers/spoonfeed/menu_items_controller.rb
@@ -3,7 +3,15 @@ class Spoonfeed::MenuItemsController < ApplicationController
   before_filter :require_user
 
   def index
-    @menu_items = MenuItem.from_premium_restaurants.all(:order => "created_at DESC").paginate(:page => params[:page])
+    if params[:keyword].present?
+      @menu_items = MenuItem.from_premium_restaurants.all(:joins => { :menu_item_keywords => :otm_keyword },
+                                 :conditions => ["otm_keywords.name = ?", params[:keyword]],
+                                 :order => "menu_items.created_at DESC")
+    else
+      @menu_items = MenuItem.from_premium_restaurants.all(:order => "created_at DESC")
+    end
+
+    @menu_items = @menu_items.paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
