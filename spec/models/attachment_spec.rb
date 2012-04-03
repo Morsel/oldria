e19a389798fb3fa018a80a86a2b78dc04abc5c8f
Audrey Eschright <spinnerin@gmail.com>
2012-04-02 17:10:10 -0700

    Adding validation to photo file names.

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -3,12 +3,17 @@ require 'spec/spec_helper'
 describe Attachment do
   before(:each) do
     @valid_attributes = {
-      :attachment_file_name => "value for attachment_file_name"
+      :attachment_file_name => "attachment_file_name.pdf"
     }
   end
 
   it "should create a new instance given valid attributes" do
     Attachment.create!(@valid_attributes)
+  end
+
+  it "should fail to create an attachment with special characters in the name" do
+    a = Attachment.new(:attachment_file_name => "©+Huber++King_Crab_with_Ossetra_Caviar_and_Fennel+closer.jpg")
+    a.should_not be_valid
   end
 end
 
