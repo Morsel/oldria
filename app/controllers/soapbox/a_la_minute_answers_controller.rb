c24653972d37cc512b986f07619f0f007dee6333
Audrey Eschright <spinnerin@gmail.com>
2012-04-16 16:11:21 -0700

    Adding other ALM answers to Soapbox page.

--- app/controllers/soapbox/a_la_minute_answers_controller.rb
+++ app/controllers/soapbox/a_la_minute_answers_controller.rb
@@ -4,6 +4,7 @@ class Soapbox::ALaMinuteAnswersController < ApplicationController
     @answer = ALaMinuteAnswer.find(params[:id])
     @question = @answer.a_la_minute_question
     @sidebar_questions = ALaMinuteQuestion.all(:order => "question")
+    @other_answers = ALaMinuteAnswer.for_question(@question).from_premium_responders.all(:order => "created_at DESC", :limit => 10).reject { |a| a == @answer }
   end
 
 end
