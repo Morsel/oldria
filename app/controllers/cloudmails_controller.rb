37a882f87620d1fd443a91fea84ff274a2cb49b9
Audrey Eschright <spinnerin@gmail.com>
2012-04-19 16:30:40 -0700

    Fix.

--- app/controllers/cloudmails_controller.rb
+++ app/controllers/cloudmails_controller.rb
@@ -202,8 +202,8 @@ class CloudmailsController < ApplicationController
     
   # sign the params, and check they actually came from cloudmailin
   def verify_cloudmail_signature
-    provided = request.request_parameters.delete(:signature).delete(:attachments)
-    signature = Digest::MD5.hexdigest(request.request_parameters.sort.map{|k,v| v}.join + CLOUDMAIL_SECRET)
+    provided = request.request_parameters.delete(:signature)
+    signature = Digest::MD5.hexdigest(request.request_parameters.except(:attachments).sort.map{ |k,v| v }.join + CLOUDMAIL_SECRET)
 
     if provided != signature
       render :text => "Message signature fail #{provided} != #{signature}", :status => 403
