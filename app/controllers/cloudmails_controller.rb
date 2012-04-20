64d5d0af90d2c11586d0c9f55ee8a74ac810e61e
Audrey Eschright <spinnerin@gmail.com>
2012-04-20 09:38:02 -0700

    Updating cloudmailin signature checking with their recommendation.

--- app/controllers/cloudmails_controller.rb
+++ app/controllers/cloudmails_controller.rb
@@ -203,12 +203,26 @@ class CloudmailsController < ApplicationController
   # sign the params, and check they actually came from cloudmailin
   def verify_cloudmail_signature
     provided = request.request_parameters.delete(:signature)
-    signature = Digest::MD5.hexdigest(request.request_parameters.except(:attachments).sort.map{ |k,v| v }.join + CLOUDMAIL_SECRET)
+    signature = Digest::MD5.hexdigest(flatten_params(request.request_parameters).sort.map { |k,v| v }.join + CLOUDMAIL_SECRET)
 
     if provided != signature
       render :text => "Message signature fail #{provided} != #{signature}", :status => 403
       return false
     end
+  end
+
+  def flatten_params(params, title = nil, result = {})
+    params.each do |key, value|
+      if value.kind_of?(Hash)
+        key_name = title ? "#{title}[#{key}]" : key
+        flatten_params(value, key_name, result)
+      else
+        key_name = title ? "#{title}[#{key}]" : key
+        result[key_name] = value
+      end
+    end
+
+    return result
   end
 
 end
