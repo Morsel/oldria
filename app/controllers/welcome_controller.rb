1d01a973bbdc1fdd5a569b9973a4ce23d01d0c12
Audrey Eschright <spinnerin@gmail.com>
2012-06-01 14:25:30 -0700

    Filtering basic restaurant content from Dashboard view for media users.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -64,12 +64,19 @@ class WelcomeController < ApplicationController
   end
 
   def set_up_dashboard_with_pagination
-    soapbox_comments   = SoapboxEntry.published.all(:order => "published_at DESC", :conditions => ["created_at > ?", 2.weeks.ago]).map(&:comments)
-    answers            = ProfileAnswer.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
-    menu_items         = MenuItem.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
-    promotions         = Promotion.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
-    restaurant_answers = RestaurantAnswer.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
-
+    if current_user.media?
+      soapbox_comments   = SoapboxEntry.published.all(:order => "published_at DESC", :conditions => ["created_at > ?", 2.weeks.ago]).map(&:comments)
+      answers            = ProfileAnswer.from_premium_users.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
+      menu_items         = MenuItem.from_premium_restaurants.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
+      promotions         = Promotion.from_premium_restaurants.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
+      restaurant_answers = RestaurantAnswer.from_premium_restaurants.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])      
+    else
+      soapbox_comments   = SoapboxEntry.published.all(:order => "published_at DESC", :conditions => ["created_at > ?", 2.weeks.ago]).map(&:comments)
+      answers            = ProfileAnswer.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
+      menu_items         = MenuItem.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
+      promotions         = Promotion.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
+      restaurant_answers = RestaurantAnswer.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
+    end
     all_comments = [soapbox_comments, answers, menu_items, promotions, restaurant_answers].flatten.sort { |a,b| b.created_at <=> a.created_at }
 
     all_comments.slice!(0..(@per_page - 1)) #delete recent
@@ -81,20 +88,35 @@ class WelcomeController < ApplicationController
   # load recent comments for dashboard
   # this data is common for all users
   def load_recent_comments
-    soapbox_comments   = SoapboxEntry.published.all(:limit => @per_page, :order => "published_at DESC").map(&:comments)
-    answers            = ProfileAnswer.all(:limit => @per_page, :order => "created_at DESC")
-    menu_items         = MenuItem.all(:limit => @per_page, :order => "created_at DESC")
-    promotions         = Promotion.all(:limit => @per_page, :order => "created_at DESC")
-    restaurant_answers = RestaurantAnswer.all(:limit => @per_page, :order => "created_at DESC")
+    if current_user.media?
+      soapbox_comments   = SoapboxEntry.published.all(:limit => @per_page, :order => "published_at DESC").map(&:comments)
+      answers            = ProfileAnswer.from_premium_users.all(:limit => @per_page, :order => "created_at DESC")
+      menu_items         = MenuItem.from_premium_restaurants.all(:limit => @per_page, :order => "created_at DESC")
+      promotions         = Promotion.from_premium_restaurants.all(:limit => @per_page, :order => "created_at DESC")
+      restaurant_answers = RestaurantAnswer.from_premium_restaurants.all(:limit => @per_page, :order => "created_at DESC")      
+    else
+      soapbox_comments   = SoapboxEntry.published.all(:limit => @per_page, :order => "published_at DESC").map(&:comments)
+      answers            = ProfileAnswer.all(:limit => @per_page, :order => "created_at DESC")
+      menu_items         = MenuItem.all(:limit => @per_page, :order => "created_at DESC")
+      promotions         = Promotion.all(:limit => @per_page, :order => "created_at DESC")
+      restaurant_answers = RestaurantAnswer.all(:limit => @per_page, :order => "created_at DESC")
+    end
 
     [soapbox_comments, answers, menu_items, promotions, restaurant_answers].flatten.sort { |a,b| b.created_at <=> a.created_at }[0..(@per_page - 1)]
   end
 
   def has_more?
-    answer_count            = ProfileAnswer.count(:conditions => ["created_at > ?", 2.weeks.ago])
-    menu_item_count         = MenuItem.count(:conditions => ["created_at > ?", 2.weeks.ago])
-    promotion_count         = Promotion.count(:conditions => ["created_at > ?", 2.weeks.ago])
-    restaurant_answer_count = RestaurantAnswer.count(:conditions => ["created_at > ?", 2.weeks.ago])
+    if current_user.media?
+      answer_count            = ProfileAnswer.from_premium_users.count(:conditions => ["profile_answers.created_at > ?", 2.weeks.ago])
+      menu_item_count         = MenuItem.from_premium_restaurants.count(:conditions => ["menu_items.created_at > ?", 2.weeks.ago])
+      promotion_count         = Promotion.from_premium_restaurants.count(:conditions => ["promotions.created_at > ?", 2.weeks.ago])
+      restaurant_answer_count = RestaurantAnswer.from_premium_restaurants.count(:conditions => ["restaurant_answers.created_at > ?", 2.weeks.ago])
+    else
+      answer_count            = ProfileAnswer.count(:conditions => ["created_at > ?", 2.weeks.ago])
+      menu_item_count         = MenuItem.count(:conditions => ["created_at > ?", 2.weeks.ago])
+      promotion_count         = Promotion.count(:conditions => ["created_at > ?", 2.weeks.ago])
+      restaurant_answer_count = RestaurantAnswer.count(:conditions => ["created_at > ?", 2.weeks.ago])
+    end
     total_count             = answer_count + menu_item_count + promotion_count + restaurant_answer_count
 
     total_count > @per_page ||
