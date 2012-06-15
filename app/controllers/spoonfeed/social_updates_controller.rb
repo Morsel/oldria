41ad807e9cafd740b82c272a8e91ff323cac003e
Audrey Eschright <spinnerin@gmail.com>
2012-06-15 13:26:13 -0700

    Temporarily changing cache duration.

--- app/controllers/spoonfeed/social_updates_controller.rb
+++ app/controllers/spoonfeed/social_updates_controller.rb
@@ -23,7 +23,7 @@ class Spoonfeed::SocialUpdatesController < ApplicationController
 
   def fetch_updates(search_params = {})
     cache_key = search_params.present? ? "filtered" : "default"
-    Rails.cache.fetch("social_updates_#{cache_key}", :expires_in => 1.hour) do
+    Rails.cache.fetch("social_updates_#{cache_key}", :expires_in => 1.minute) do
       alm_answers = ALaMinuteAnswer.social_results(search_params)
 
       twitter_posts = []
