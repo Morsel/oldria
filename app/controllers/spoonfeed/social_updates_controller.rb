5d16897a6f2fe6041906be65352d9281f9a0b81e
Audrey Eschright <spinnerin@gmail.com>
2012-06-15 17:03:00 -0700

    Redo.

--- app/controllers/spoonfeed/social_updates_controller.rb
+++ app/controllers/spoonfeed/social_updates_controller.rb
@@ -12,12 +12,12 @@ class Spoonfeed::SocialUpdatesController < ApplicationController
     render :partial => "updates"
   end
 
-  def filter_updates
-    sorted_merge = fetch_updates
-
-    @updates = sorted_merge.paginate(:page => params[:page], :per_page => 10)
-    render :partial => "updates"
-  end
+  # def filter_updates
+  #   sorted_merge = fetch_updates(params[:search])
+  # 
+  #   @updates = sorted_merge.paginate(:page => params[:page], :per_page => 10)
+  #   render :partial => "updates"
+  # end
 
   private
 
