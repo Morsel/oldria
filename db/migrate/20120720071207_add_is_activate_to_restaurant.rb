d4b3ce0bf31dc9a2feff844c570b14eb2b83a507
Nishant Nigam <nishant.n@cisinlabs.com>
2012-07-26 13:40:52 +0530

    Update existing restaurnat is_activated flag to true

--- db/migrate/20120720071207_add_is_activate_to_restaurant.rb
+++ db/migrate/20120720071207_add_is_activate_to_restaurant.rb
@@ -1,6 +1,8 @@
 class AddIsActivateToRestaurant < ActiveRecord::Migration
   def self.up
-    add_column :restaurants, :is_activated, :boolean,:default => false 
+    add_column :restaurants, :is_activated, :boolean,:default => false   
+    #Restaurant.update_all("is_activated = true")
+    execute "UPDATE restaurants set is_activated = true" 
   end
 
   def self.down
