4fc1382b055876ec3e57b358fe08b164c837d46e
Audrey Eschright <spinnerin@gmail.com>
2012-07-25 17:15:42 -0700

    Passing test for ALM Answers controller - twitter crossposting.

--- spec/controllers/a_la_minute_answers_controller_spec.rb
+++ spec/controllers/a_la_minute_answers_controller_spec.rb
@@ -3,16 +3,16 @@ require 'spec_helper'
 describe ALaMinuteAnswersController do
   include ActionView::Helpers::RecordIdentificationHelper
 
-  let(:restaurant) { Factory(:restaurant) }
-  let(:current_user) { Factory(:user) }
-  let(:question) { Factory(:a_la_minute_question) }
-
-  before(:each) do
-    Factory(:employment, :restaurant => restaurant, :employee => current_user)
-    controller.stubs(:current_user).returns current_user
-  end
-
   describe "PUT bulk_update" do
+    let(:restaurant) { Factory(:restaurant) }
+    let(:current_user) { Factory(:user) }
+    let(:question) { Factory(:a_la_minute_question) }
+
+    before(:each) do
+      Factory(:employment, :restaurant => restaurant, :employee => current_user)
+      controller.stubs(:current_user).returns current_user
+    end
+
     describe "successful update" do
       before(:each) do
         previous_answer = ALaMinuteAnswer.create(:answer => "old answer",
@@ -50,26 +50,35 @@ describe ALaMinuteAnswersController do
       specify { question.should have(1).a_la_minute_answers }
     end
 
-    describe "update with cross-posting" do
+  end
 
-      it "should crosspost to Twitter" do
-        restaurant = Factory(:restaurant, :atoken => "qwerty", :asecret => "1234567", :facebook_page_id => "987987213", :facebook_page_token => "kljas987as")
-        twitter_client = mock
-        restaurant.stubs(:twitter_client).returns(twitter_client)
-        twitter_client.expects(:send_at).returns(true)
+  describe "update with cross-posting" do
 
-        previous_answer = Factory(:a_la_minute_answer, :responder => restaurant, :a_la_minute_question => question)
+    before(:each) do
+      @user = Factory(:user)
+      @restaurant = Factory(:restaurant, :atoken => "qwerty", :asecret => "1234567", :facebook_page_id => "987987213", :facebook_page_token => "kljas987as")
+      Factory(:employment, :restaurant => @restaurant, :employee => @user)
+      controller.stubs(:current_user).returns @user
+    end
 
-        put :bulk_update, "restaurant_id" => restaurant.id,
-        "a_la_minute_questions" => {
-          question.id.to_s => {
-            "answer" => "new answer",
-            "answer_id" => previous_answer.id,
-            "post_to_twitter_at" => Time.now
-          }
+    it "should crosspost to Twitter" do
+      twitter_client = mock
+      Restaurant.any_instance.expects(:twitter_client).returns(twitter_client)
+      twitter_client.expects(:send_at).returns(true)
+
+      question = Factory(:a_la_minute_question)
+      previous_answer = Factory(:a_la_minute_answer, :answer => "old answer", :responder => @restaurant, :a_la_minute_question => question, :created_at => 1.day.ago)
+
+      put :bulk_update, "restaurant_id" => @restaurant.id,
+      "a_la_minute_questions" => {
+        question.id.to_s => {
+          "answer" => "new cross-posting answer",
+          "answer_id" => previous_answer.id,
+          "post_to_twitter_at" => Time.now
         }
-      end
+      }
 
+      question.reload.should have(2).a_la_minute_answers
     end
 
   end
