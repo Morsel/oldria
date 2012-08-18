8ca276fb6cc9588d507dd3f5403758fa1b4bf60a
Audrey Eschright <spinnerin@gmail.com>
2012-08-17 18:18:45 -0700

    Adding form for restaurants to set newsletter frequency.

--- config/routes.rb
+++ config/routes.rb
@@ -215,7 +215,7 @@ ActionController::Routing::Routes.draw do |map|
     restaurant.btl_topic 'behind_the_line/topic/:id', :controller => 'restaurants/behind_the_line', :action => 'topic'
     restaurant.btl_chapter 'behind_the_line/chapter/:id', :controller => 'restaurants/behind_the_line', :action => 'chapter'
 
-    restaurant.resources :newsletters, :controller => 'restaurants/newsletters'
+    restaurant.resources :newsletters, :controller => 'restaurants/newsletters', :collection => { :update_settings => :post }
   end
 
   map.resources :user_sessions, :password_resets, :followings, :pages
