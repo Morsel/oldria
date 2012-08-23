cf262a21a5f55cb44708bd60309624bba79213c2
Nishant Nigam <nishant.n@cisinlabs.com>
2012-08-23 19:20:41 +0530

    A La Minute side bar chnages

--- app/controllers/soapbox/a_la_minute_questions_controller.rb
+++ app/controllers/soapbox/a_la_minute_questions_controller.rb
@@ -8,7 +8,7 @@ class Soapbox::ALaMinuteQuestionsController < ApplicationController
   def show
     @question = ALaMinuteQuestion.find(params[:id])
     @answers =  ALaMinuteAnswer.from_premium_responders.newest_for(@question)
-    @answers = @answers.paginate(:page => params[:page], :per_page => 5)
+    @answers = @answers.paginate(:page => params[:page], :per_page => 10)
     @sidebar_questions = ALaMinuteQuestion.all(:order => "question")
   end
 
