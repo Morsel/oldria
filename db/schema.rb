e7a3e28830eb9b4b97b15d0011870f5ba33379e9
Audrey Eschright <spinnerin@gmail.com>
2012-08-27 15:33:14 -0700

    Synchronizing restaurant newsletter subscribers with MailChimp.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120817183017) do
+ActiveRecord::Schema.define(:version => 20120823175108) do
 
   create_table "a_la_minute_answers", :force => true do |t|
     t.text     "answer"
--- spec/controllers/soapbox/restaurants_controller_spec.rb
+++ spec/controllers/soapbox/restaurants_controller_spec.rb
@@ -28,24 +28,32 @@ describe Soapbox::RestaurantsController do
 
   describe "subscribing to a restaurant's newsletter" do
 
+    before(:each) do
+      @restaurant = Factory(:restaurant)
+      @subscriber = Factory(:newsletter_subscriber)
+      
+      MailchimpConnector.stubs(:new).returns(mc = mock)
+      mc.stubs(:client).returns(client = mock)
+      mc.stubs(:mailing_list_id).returns(1234)
+
+      client.stubs(:list_interest_groupings).returns("#{@restaurant.name} in #{@restaurant.city} #{@restaurant.state}")
+      client.stubs(:list_update_member).returns(true)
+    end
+
     it "should allow a newsletter subscriber to add a restaurant subscription" do
-      restaurant = Factory(:restaurant)
-      subscriber = Factory(:newsletter_subscriber)
-      cookies['newsletter_subscriber_id'] = subscriber.id
+      cookies['newsletter_subscriber_id'] = @subscriber.id
       NewsletterSubscription.count.should == 0
 
-      post :subscribe, :id => restaurant.id
+      post :subscribe, :id => @restaurant.id
       NewsletterSubscription.count.should == 1
     end
 
     it "should allow a newsletter subscriber to remove a restaurant subscription" do
-      restaurant = Factory(:restaurant)
-      subscriber = Factory(:newsletter_subscriber)
-      cookies['newsletter_subscriber_id'] = subscriber.id
-      NewsletterSubscription.create(:restaurant => restaurant, :newsletter_subscriber => subscriber)
+      cookies['newsletter_subscriber_id'] = @subscriber.id
+      NewsletterSubscription.create(:restaurant => @restaurant, :newsletter_subscriber => @subscriber)
       NewsletterSubscription.count.should == 1
 
-      post :unsubscribe, :id => restaurant.id, :subscriber_id => subscriber.id
+      post :unsubscribe, :id => @restaurant.id, :subscriber_id => @subscriber.id
       NewsletterSubscription.count.should == 0
     end
 
