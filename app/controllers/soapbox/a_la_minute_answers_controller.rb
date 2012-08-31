35f5af775db201654fcb54d8b1ab2a49770621eb
Nishant Nigam <nishant.n@cisinlabs.com>
2012-08-31 15:01:21 +0530

    Fixed : Stories : 35121753 , Added Latest news/otm from the restaurant below on the A la Minute show page

--- app/controllers/soapbox/a_la_minute_answers_controller.rb
+++ app/controllers/soapbox/a_la_minute_answers_controller.rb
@@ -3,8 +3,11 @@ class Soapbox::ALaMinuteAnswersController < ApplicationController
   def show
     @answer = ALaMinuteAnswer.find(params[:id])
     @question = @answer.a_la_minute_question
-    @sidebar_questions = ALaMinuteQuestion.all(:order => "question")
-    @other_answers = ALaMinuteAnswer.for_question(@question).from_premium_responders.all(:order => "created_at DESC", :limit => 10).reject { |a| a == @answer }
+    @sidebar_questions = ALaMinuteQuestion.all(:order => "question")        
+    @promotions = @answer.restaurant.promotions.all\
+    	(:conditions=>["DATE(created_at) > DATE(?) or DATE(created_at) = DATE(?) ", Time.now,Time.now],:order => "created_at DESC",:limit=>3)
+    @menu_items = @answer.restaurant.menu_items.all\
+      (:conditions=>["DATE(created_at) < DATE(?)", Time.now],:limit=>3,:order=>"menu_items.created_at DESC")
   end
 
 end
