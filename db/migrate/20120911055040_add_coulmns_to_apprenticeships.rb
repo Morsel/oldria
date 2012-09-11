e384366328f4731ffb0dfa0baf18bc1e50728256
Nishant Nigam <nishant.n@cisinlabs.com>
2012-09-11 11:57:32 +0530

    Stories :: 35141061 :: Fixed apprenticeships in personal profiles have odd date fields

---
+++ db/migrate/20120911055040_add_coulmns_to_apprenticeships.rb
@@ -0,0 +1,14 @@
+class AddCoulmnsToApprenticeships < ActiveRecord::Migration
+  def self.up
+    add_column :apprenticeships, :start_date, :date
+    add_column :apprenticeships, :end_date, :date
+    Apprenticeship.all.each do |row|
+      row.update_attribute(:start_date, Time.now)
+    end
+  end
+
+  def self.down
+    remove_column :apprenticeships, :end_date
+    remove_column :apprenticeships, :start_date
+  end
+end
