f8a83adee301fdaf79dd62de91dea5695f6e1ab7
Joshua Suggs <joshsuggs@gmail.com>
2012-10-03 10:33:35 -0400

    [#37073131] User.find_using_perishable_token always returns nil. Use User.find_by_perishable_token instead.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -49,7 +49,7 @@ class PasswordResetsController < ApplicationController
   private
 
   def load_user_using_perishable_token
-    @user = User.find_using_perishable_token(params[:id])
+    @user = User.find_by_perishable_token(params[:id])
     unless @user
       flash[:notice] = "Oops. We're having trouble finding your account."
       redirect_to :action => "new"
