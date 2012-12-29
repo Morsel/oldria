4338f905d4484223857c7311bbd428b9fd34c0d9
Nishant Nigam <nishant.n@cisinlabs.com>
2012-12-29 15:17:22 +0530

    [#41710075] Added comming soon for Archives

--- app/controllers/restaurants/newsletters_controller.rb
+++ app/controllers/restaurants/newsletters_controller.rb
@@ -61,6 +61,9 @@ class Restaurants::NewslettersController < ApplicationController
   end
 
   def archives    
+    unless [56,146,250,67,269,17].include? params[:restaurant_id].to_i
+      render "restaurants/_comming_soon"
+    end
   end
     
   private
