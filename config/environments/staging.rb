637576ecf67655a1c1dade919fe73f1f43cc8a2d
Nishant Nigam <nishant.n@cisinlabs.com>
2013-01-03 20:20:52 +0530

    [#41695393] changed the mailchimp key for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -42,4 +42,4 @@ CLOUDMAIL_DOMAIN = 'staging-mailbot.restaurantintelligenceagency.com'
 # for verifying the emails came from cloudmailin
 CLOUDMAIL_SECRET = '72fa425b1e5c89239639'.freeze
 
-ENV['MC_API_KEY'] = 'b992d9bf14db221d322b7add975b1c39-us2'
+ENV['MC_API_KEY'] = '55b09bb8257774874d2c9427532ba067-us6'
