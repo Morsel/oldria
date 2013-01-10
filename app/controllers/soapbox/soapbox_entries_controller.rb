3aaff989fa6effbc16d5a289b1fa087f63aa1cf3
Nishant Nigam <nishant.n@cisinlabs.com>
2013-01-10 19:56:48 +0530

    [#42285055] new action added for frontburner

--- app/controllers/soapbox/soapbox_entries_controller.rb
+++ app/controllers/soapbox/soapbox_entries_controller.rb
@@ -11,22 +11,11 @@ class Soapbox::SoapboxEntriesController < Soapbox::SoapboxController
   end
 
   def index
-    @announcements = []
-
-    @soapbox_comments   = SoapboxEntry.published.all(:limit => @per_page, :order => "published_at DESC").map(&:comments)
-    @answers            = ProfileAnswer.from_premium_users.all(:limit => @per_page, :order => "created_at DESC")
-    @menu_items         = MenuItem.activated_restaurants.from_premium_restaurants.all(:limit => @per_page, :order => "menu_items.created_at DESC")
-    @promotions         = Promotion.from_premium_restaurants.all(:limit => @per_page, :order => "created_at DESC")
-    @restaurant_answers = RestaurantAnswer.activated_restaurants.from_premium_restaurants.all(:limit => @per_page, :order => "restaurant_answers.created_at DESC")
-
-
-
     @main_feature = SoapboxEntry.main_feature
     @main_feature_comments = SoapboxEntry.main_feature_comments if @main_feature
 
     @secondary_feature = SoapboxEntry.secondary_feature
     @secondary_feature_comments = SoapboxEntry.secondary_feature_comments if @secondary_feature
-    render 'dashboard'
   end
 
   def dashboard_cache_key
@@ -78,6 +67,24 @@ class Soapbox::SoapboxEntriesController < Soapbox::SoapboxController
     render :action => "show"
   end
 
+  def frontburner
+    @announcements = []
+
+    @soapbox_comments   = SoapboxEntry.published.all(:limit => @per_page, :order => "published_at DESC").map(&:comments)
+    @answers            = ProfileAnswer.from_premium_users.all(:limit => @per_page, :order => "created_at DESC")
+    @menu_items         = MenuItem.activated_restaurants.from_premium_restaurants.all(:limit => @per_page, :order => "menu_items.created_at DESC")
+    @promotions         = Promotion.from_premium_restaurants.all(:limit => @per_page, :order => "created_at DESC")
+    @restaurant_answers = RestaurantAnswer.activated_restaurants.from_premium_restaurants.all(:limit => @per_page, :order => "restaurant_answers.created_at DESC")
+
+
+
+    @main_feature = SoapboxEntry.main_feature
+    @main_feature_comments = SoapboxEntry.main_feature_comments if @main_feature
+
+    @secondary_feature = SoapboxEntry.secondary_feature
+    @secondary_feature_comments = SoapboxEntry.secondary_feature_comments if @secondary_feature
+    render 'dashboard'
+  end
   protected
 
   def hide_flashes
