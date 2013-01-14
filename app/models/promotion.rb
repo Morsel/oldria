9beaadc60597e69153be49a055e5d790667895c1
Nishant Nigam <nishant.n@cisinlabs.com>
2013-01-14 20:17:57 +0530

    [#42431827] Removed the html charectar and add new line chrectar

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -143,7 +143,7 @@ class Promotion < ActiveRecord::Base
       :message     => message,
       :link        => soapbox_promotion_url(self),
       :name        => headline,
-      :description => details.gsub(/(<[^>]*>)|\r|\n|\t/s) {" "} ,
+      :description => Loofah::Helpers.strip_tags(details.gsub(/(<[^>]*>)|\r|\t/s) {" "}), 
       :picture => (restaurant.logo && restaurant.logo.attachment?) ? restaurant.logo.attachment.url(:medium) : nil
     }
     restaurant.post_to_facebook_page(post_attributes)
