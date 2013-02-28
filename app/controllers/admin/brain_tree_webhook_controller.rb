9ede17bfa6e8533336b0ec23b856193b90aef0da
Nishant Nigam <nishant.n@cisinlabs.com>
2013-02-28 15:12:22 +0530

    [#44798845] Changed mail tamplate

--- app/controllers/admin/brain_tree_webhook_controller.rb
+++ app/controllers/admin/brain_tree_webhook_controller.rb
@@ -5,21 +5,25 @@ class Admin::BrainTreeWebhookController < ApplicationController
 		Rails.logger.error("Braintree params #{params}")
 
 		unless params[:bt_signature].blank?
-				webhook_notification = Braintree::WebhookNotification.parse(
-				  params[:bt_signature], params[:bt_payload]
-				)
-				
-				if webhook_notification.kind == "subscription_charged_unsuccessfully"
-						subscriber = Subscription.find_by_braintree_id(webhook_notification.subscription.id)
-						unless subscriber.blank?
-							UserMailer.deliver_send_payment_error(subscriber.subscriber.try(:name).try(:capitalize),"Subscription get failed")
-						end	
-				end	
-			else
-				UserMailer.deliver_send_payment_error("Nishant nigam","Brain tree code get faild, Please  frwd to nishant")
+			 webhook_notification = Braintree::WebhookNotification.parse(
+                                 params[:bt_signature], params[:bt_payload]
+             )
+
+			if webhook_notification.kind == "subscription_charged_unsuccessfully"
+				subscriber = Subscription.find_by_braintree_id(webhook_notification.subscription.id)
+					if !subscriber.blank? && !subscriber.subscriber.blank?
+						if subscriber.subscriber_type == "Restaurant"
+							link =  edit_restaurant_url(subscriber.subscriber)
+						else
+							link =  edit_user_profile_url(subscriber.subscriber)
+						end
+						UserMailer.deliver_send_braintree_payment_error(subscriber.subscriber.try(:name).try(:capitalize),link)
+					end	
+			end	
+		else
+				UserMailer.deliver_send_braintree_payment_error("Nishant nigam","No link")
 		end	
-			render :text =>Braintree::WebhookNotification.verify(params[:bt_challenge])
+		render :text =>Braintree::WebhookNotification.verify(params[:bt_challenge])
 	end	
 
-	
 end
