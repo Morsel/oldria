5b653b26a9088f8262b93466b640786c20997480
Nishant Nigam <nishant.n@cisinlabs.com>
2013-03-28 21:50:30 +0530

    [#46574875] merged the resturant visitor array with page_view vistors

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -253,9 +253,10 @@ class RestaurantsController < ApplicationController
     end
     redirect_to root_path
   end
-  def restaurant_visitors
-      redirect_to :action => :edit
-      #@visitors = @restaurant.user_restaurant_visitors # will be use when redirect_to removed
+  def restaurant_visitors      
+      @visitors = []
+      visitors = @restaurant.page_views.map(&:user).uniq!      
+      @visitors = visitors.zip(@restaurant.restaurant_visitors).flatten.compact # old was .user_restaurant_visitors
   end
   def media_subscribe     
     if current_user.media?   
