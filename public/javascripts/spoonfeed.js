3bc7e1404e233757766ce9b32ac1ed04303a52e4
nishant-n <cis@CISM467.(none)>
2013-07-11 12:24:23 +0530

    invite more than 5 user

--- public/javascripts/spoonfeed.js
+++ public/javascripts/spoonfeed.js
@@ -655,6 +655,56 @@ $('#metropolitan_areas_state_state_id,#digest_metropolitan_areas_state_state_id'
     else
       $(this).prev().attr("disabled","disabled");
   })
+ $('#add_more').click(function(e){
+  e.preventDefault();
+  more_people = $('#more_people').clone();
+  $('#more_people').show();
+  $(".recommendation_invitations .name").each(function() {
+      $(this).removeClass('name');
+      $(this).addClass('first_name');
+  });
+  $(".recommendation_invitations .last").each(function() {
+      $(this).removeClass('last');
+      $(this).addClass('last_name');
+  });
+  $(".recommendation_invitations .email").each(function() {
+      $(this).removeClass('email');
+      $(this).addClass('email_id');
+  });
+  more_people.insertAfter('#more_people');
+  // $(this).parent.after('<br/>');
+      // $(this).text('close');
+      //$(this).after().append("<a href='#' id='more_people'>Add more people</a>")
+
+  });
+  $('#recommendation_invitations').click(function(e){
+    e.preventDefault(); 
+    var ret_val=""
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    $(".recommendation_invitations .first_name").each(function() {
+      if($(this).val()==""){
+        ret_val='Please enter the first name<br/>';
+        return false
+      }
+    });
+    $(".recommendation_invitations .last_name").each(function() {
+        if($(this).val()==""){
+        ret_val=ret_val+'Please enter the last name<br/>';
+        return false
+      }
+    });
+    $(".recommendation_invitations .email_id").each(function() {
+       if($(this).val()=="" || !pattern.test($(this).val()) ){
+        ret_val=ret_val+'Please enter the valid email id<br/>';
+        return false
+      }
+    });    
+    if (ret_val.length==0){
+      $("#form_invitation").submit();
+    }else{
+      $('#flashes').html('<div id="flash_error">'+ret_val+'</div>');
+    }
+  });
   // end $(document).ready
 });
 
