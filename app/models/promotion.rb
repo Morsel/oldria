eb1fe50d1ae696844ef9ddd7ca4bd47ae2f6acec
Nishant Nigam <nishant.n@cisinlabs.com>
2013-07-20 19:33:50 +0530

    added column space

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -160,7 +160,7 @@ class Promotion < ActiveRecord::Base
       campaign_id = \
       mc.client.campaign_create(:type => "regular",
                                 :options => { :list_id => mc.media_promotion_list_id,
-                                              :subject => "#{self.promotion_type.try(:name)}:#{self.headline}",
+                                              :subject => "#{self.promotion_type.try(:name)}: #{self.headline}",
                                               :from_email => "info@restaurantintelligenceagency.com",
                                               :to_name => "*|FNAME|*",
                                               :from_name => "Restaurant Intelligence Agency",
