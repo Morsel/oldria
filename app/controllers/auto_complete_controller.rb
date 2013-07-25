e4f08f1623c3d621251673410a35dcf1e4f4ff37
nishant-n <cis@CISM467.(none)>
2013-07-25 20:39:22 +0530

    change the layout serach result

--- app/controllers/auto_complete_controller.rb
+++ app/controllers/auto_complete_controller.rb
@@ -1,10 +1,10 @@
 class AutoCompleteController < ApplicationController
 
   def index
-    if params[:soapbox] && params[:person]
+    if params[:person]
       respond_to do |format|
         format.js { auto_complete_person_keywords }
-      end   
+      end
     else
       respond_to do |format|
         format.js { auto_complete_keywords }
@@ -13,20 +13,20 @@ class AutoCompleteController < ApplicationController
   end
 
   def auto_complete_keywords
-    keyword_name = params[:term]    
+    keyword_name = params[:term]
     if params[:name]=="restaurant"
-      @keywords = ["RESTAURANT BY OTM"] + OtmKeyword.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 2).map(&:name) + ["RESTAURANT BY NAME"] + Restaurant.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 2).map(&:name) + ["RESTAURANT BY FEATURE"] + RestaurantFeature.find(:all,:conditions => ["value like ?", "%#{keyword_name}%"],:limit => 2).map(&:value) + ["RESTAURANT BY CUISINE"]  + Cuisine.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 2).map(&:name) 
+      @keywords = ["RESTAURANTS BY KEYWORD"] + OtmKeyword.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 2).map(&:name) + ["RESTAURANTS BY NAME"] + Restaurant.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 2).map(&:name) + ["RESTAURANTS BY FEATURE"] + RestaurantFeature.find(:all,:conditions => ["value like ?", "%#{keyword_name}%"],:limit => 2).map(&:value) + ["RESTAURANTS BY CUISINE"] + Cuisine.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 2).map(&:name)
     else
-      @keywords = ["RESTAURANT BY REGION"]  + JamesBeardRegion.find(:all,:conditions => ["name like ? or description like ?", "%#{keyword_name}%","%#{keyword_name}%"],:limit => 5).map(&:name) + ["RESTAURANT BY STATE"] + get_state_name_array.grep(/^#{keyword_name}/i)[0..5]
+      @keywords = ["RESTAURANTS BY REGION"] + JamesBeardRegion.find(:all,:conditions => ["name like ? or description like ?", "%#{keyword_name}%","%#{keyword_name}%"],:limit => 5).map(&:name) + ["RESTAURANTS BY STATE"] + get_state_name_array.grep(/^#{keyword_name}/i)[0..5]
     end
-    remove_value = ["RESTAURANT BY CUISINE","RESTAURANT BY STATE","RESTAURANT BY OTM","RESTAURANT BY REGION","RESTAURANT BY FEATURE","RESTAURANT BY NAME"]
+    remove_value = ["RESTAURANTS BY CUISINE","RESTAURANTS BY STATE","RESTAURANTS BY OTM","RESTAURANTS BY REGION","RESTAURANTS BY FEATURE","RESTAURANTS BY NAME"]
     test_result_search = @keywords.clone
     remove_value.each do |dels|
        test_result_search.delete("#{dels}")
     end
-    unless test_result_search.present?      
+    unless test_result_search.present?
       if params[:name]=="restaurant"
-        UserMailer.deliver_send_otm_keyword_notification(current_user,keyword_name) 
+        UserMailer.deliver_send_otm_keyword_notification(current_user,keyword_name)
       end
       render :json => test_result_search.push('This keyword does not yet exist in our database.')
     else
@@ -35,23 +35,23 @@ class AutoCompleteController < ApplicationController
   end
 
   def auto_complete_person_keywords
-    keyword_name = params[:term]   
+    keyword_name = params[:term]
     if params[:soapbox]
-      @keywords = ["PERSON BY NAME"] + User.in_soapbox_directory.for_autocomplete.find_all_by_name(keyword_name).map(&:name)[0..4]
+      @keywords = ["PERSONS BY NAME"] + User.in_soapbox_directory.for_autocomplete.find_all_by_name(keyword_name).map(&:name)[0..4]
     else
-      @keywords = ["PERSON BY NAME"] + User.in_spoonfeed_directory.for_autocomplete.find_all_by_name(keyword_name).map(&:name)[0..4]
+      @keywords = ["PERSONS BY NAME"] + User.in_spoonfeed_directory.for_autocomplete.find_all_by_name(keyword_name).map(&:name)[0..4]
     end
     if params[:person]=="person"
-      @keywords = @keywords + ["PERSON BY SPECIALITY"] + Specialty.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 4).map(&:name) + ["PERSON BY CUISINE"] + Cuisine.find(:all,:conditions => ["name like ?","%#{keyword_name}%"],:limit => 4).map(&:name)
+      @keywords = @keywords + ["PERSONS BY SPECIALITY"] + Specialty.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 4).map(&:name) + ["PERSONS BY CUISINE"] + Cuisine.find(:all,:conditions => ["name like ?","%#{keyword_name}%"],:limit => 4).map(&:name)
     else
-      @keywords = ["PERSON BY STATE"] + JamesBeardRegion.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 7).map(&:name) + ["PERSON BY REGION"] + MetropolitanArea.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 7).map(&:name) 
+      @keywords = ["PERSONS BY STATE"] + JamesBeardRegion.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 7).map(&:name) + ["PERSONS BY REGION"] + MetropolitanArea.find(:all,:conditions => ["name like ?", "%#{keyword_name}%"],:limit => 7).map(&:name)
     end
-    remove_value = ["PERSON BY NAME","PERSON BY SPECIALITY","PERSON BY CUISINE","PERSON BY STATE","PERSON BY REGION"]
+    remove_value = ["PERSONS BY NAME","PERSONS BY SPECIALITY","PERSONS BY CUISINE","PERSONS BY STATE","PERSONS BY REGION"]
     test_result_search = @keywords.clone
     remove_value.each do |dels|
        test_result_search.delete("#{dels}")
     end
-    unless test_result_search.present?      
+    unless test_result_search.present?
       render :json => test_result_search.push('This keyword does not yet exist in our database.')
     else
       render :json => @keywords
--- public/soapbox/_js/autocomplete-filter.js
+++ public/soapbox/_js/autocomplete-filter.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   $("#search_restaurant_eq_any_name").autocomplete({
     source: "/auto_complete.js?name=restaurant",
   }).data("autocomplete")._renderItem = function (ul, item) {
-    if(["This keyword does not yet exist in our database.","RESTAURANT BY NAME", "RESTAURANT BY OTM", "RESTAURANT BY FEATURE", "RESTAURANT BY CUISINE"].indexOf(item.label) > -1){
-         return $("<li></li>")
+    if(["This keyword does not yet exist in our database.","RESTAURANTS BY NAME", "RESTAURANTS BY KEYWORD", "RESTAURANTS BY FEATURE", "RESTAURANTS BY CUISINE"].indexOf(item.label) > -1){
+          return $("<li></li>")
              .data("item.autocomplete", item)
              .append("<font color='black'>" + item.label + "</font>")
              .appendTo(ul);
@@ -20,7 +20,7 @@ $(document).ready(function(){
   $("#search_restaurant_by_state_or_region").autocomplete({
     source: "/auto_complete.js?name=region",
   }).data("autocomplete")._renderItem = function (ul, item) {
-    if(["This keyword does not yet exist in our database.","RESTAURANT BY REGION", "RESTAURANT BY STATE"].indexOf(item.label) > -1){
+     if(["This keyword does not yet exist in our database.","RESTAURANTS BY REGION", "RESTAURANTS BY STATE"].indexOf(item.label) > -1){
          return $("<li></li>")
              .data("item.autocomplete", item)
              .append("<font color='black'>" + item.label + "</font>")
@@ -37,8 +37,8 @@ $(document).ready(function(){
   $('#search_person_eq_any_name').autocomplete({
     source: "/auto_complete.js?soapbox=soapbox&person=person"
   }).data("autocomplete")._renderItem = function (ul, item) {
-    if(["This keyword does not yet exist in our database.","PERSON BY NAME","PERSON BY SPECIALITY","PERSON BY CUISINE"].indexOf(item.label) > -1){
-         return $("<li></li>")
+    if(["This keyword does not yet exist in our database.","PERSONS BY NAME","PERSONS BY SPECIALITY","PERSONS BY CUISINE"].indexOf(item.label) > -1){
+          return $("<li></li>")
              .data("item.autocomplete", item)
              .append("<font color='black'>" + item.label + "</font>")
              .appendTo(ul);
@@ -53,7 +53,7 @@ $(document).ready(function(){
   $("#search_person_by_state_or_region").autocomplete({
     source: "/auto_complete.js?soapbox=soapbox&person=region"
   }).data("autocomplete")._renderItem = function (ul, item) {
-    if(["This keyword does not yet exist in our database.","PERSON BY STATE","PERSON BY REGION"].indexOf(item.label) > -1){
+    if(["This keyword does not yet exist in our database.","PERSONS BY STATE","PERSONS BY REGION"].indexOf(item.label) > -1){
          return $("<li></li>")
              .data("item.autocomplete", item)
              .append("<font color='black'>" + item.label + "</font>")
