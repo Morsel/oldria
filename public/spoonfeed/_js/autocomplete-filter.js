b0faf4c3a2a850f416fbe8f9e7d90a07c5cbccc0
nishant-n <cis@CISM467.(none)>
2013-07-25 19:57:51 +0530

    change the layout of search result

--- public/spoonfeed/_js/autocomplete-filter.js
+++ public/spoonfeed/_js/autocomplete-filter.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $("#search_restaurant_eq_any_name").autocomplete({
     source: "/auto_complete.js?name=restaurant",
   }).data("autocomplete")._renderItem = function (ul, item) {
-    if(["This keyword does not yet exist in our database.","RESTAURANT BY NAME", "RESTAURANT BY OTM", "RESTAURANT BY FEATURE", "RESTAURANT BY CUISINE"].indexOf(item.label) > -1){
+    if(["This keyword does not yet exist in our database.","RESTAURANTS BY NAME", "RESTAURANTS BY KEYWORD", "RESTAURANTS BY FEATURE", "RESTAURANTS BY CUISINE"].indexOf(item.label) > -1){
          return $("<li></li>")
              .data("item.autocomplete", item)
              .append("<font color='black'>" + item.label + "</font>")
@@ -20,7 +20,7 @@ $(document).ready(function(){
   $("#search_restaurant_by_state_or_region").autocomplete({
     source: "/auto_complete.js?name=region",
   }).data("autocomplete")._renderItem = function (ul, item) {
-    if(["This keyword does not yet exist in our database.","RESTAURANT BY REGION", "RESTAURANT BY STATE"].indexOf(item.label) > -1){
+    if(["This keyword does not yet exist in our database.","RESTAURANTS BY REGION", "RESTAURANTS BY STATE"].indexOf(item.label) > -1){
          return $("<li></li>")
              .data("item.autocomplete", item)
              .append("<font color='black'>" + item.label + "</font>")
@@ -37,7 +37,7 @@ $(document).ready(function(){
   $('#search_person_eq_any_name').autocomplete({
     source: "/auto_complete.js?person=person"
   }).data("autocomplete")._renderItem = function (ul, item) {
-    if(["This keyword does not yet exist in our database.","PERSON BY NAME","PERSON BY SPECIALITY","PERSON BY CUISINE"].indexOf(item.label) > -1){
+    if(["This keyword does not yet exist in our database.","PERSONS BY NAME","PERSONS BY SPECIALITY","PERSONS BY CUISINE"].indexOf(item.label) > -1){
          return $("<li></li>")
              .data("item.autocomplete", item)
              .append("<font color='black'>" + item.label + "</font>")
@@ -53,7 +53,7 @@ $(document).ready(function(){
   $("#search_person_by_state_or_region").autocomplete({
     source: "/auto_complete.js?person=region"
   }).data("autocomplete")._renderItem = function (ul, item) {
-    if(["This keyword does not yet exist in our database.","PERSON BY STATE","PERSON BY REGION"].indexOf(item.label) > -1){
+    if(["This keyword does not yet exist in our database.","PERSONS BY STATE","PERSONS BY REGION"].indexOf(item.label) > -1){
          return $("<li></li>")
              .data("item.autocomplete", item)
              .append("<font color='black'>" + item.label + "</font>")
