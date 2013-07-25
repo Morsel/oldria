97767c75fb8d42111a1fbf5abe0292f6cf760afa
nishant-n <cis@CISM467.(none)>
2013-07-25 18:01:52 +0530

    on dashboard show all restaurant events

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -66,11 +66,11 @@ class WelcomeController < ApplicationController
   def set_up_dashboard_with_pagination
     if current_user.media?
       soapbox_comments   = SoapboxEntry.published.all(:order => "published_at DESC", :conditions => ["created_at > ?", 2.weeks.ago]).map(&:comments)
-      answers            = ProfileAnswer.from_premium_users.all(:order => "profile_answers.created_at DESC", :conditions => ["profile_answers.created_at > ?", 2.weeks.ago])
-      menu_items         = MenuItem.activated_restaurants.from_premium_restaurants.all(:order => "menu_items.created_at DESC", :conditions => ["menu_items.created_at > ?", 2.weeks.ago])
-      promotions         = Promotion.from_premium_restaurants.all(:order => "promotions.created_at DESC", :conditions => ["promotions.created_at > ?", 2.weeks.ago])
-      restaurant_answers = RestaurantAnswer.from_premium_restaurants.all(:order => "restaurant_answers.created_at DESC", :conditions => ["restaurant_answers.created_at > ?", 2.weeks.ago])   
-      a_la_minutes       = ALaMinuteAnswer.from_premium_responders.all(:order => "a_la_minute_answers.created_at DESC", :conditions => ["a_la_minute_answers.created_at > ?", 2.weeks.ago])         
+      answers            = ProfileAnswer.all(:order => "profile_answers.created_at DESC", :conditions => ["profile_answers.created_at > ?", 2.weeks.ago])
+      menu_items         = MenuItem.activated_restaurants.all(:order => "menu_items.created_at DESC", :conditions => ["menu_items.created_at > ?", 2.weeks.ago])
+      promotions         = Promotion.all(:order => "promotions.created_at DESC", :conditions => ["promotions.created_at > ?", 2.weeks.ago])
+      restaurant_answers = RestaurantAnswer.all(:order => "restaurant_answers.created_at DESC", :conditions => ["restaurant_answers.created_at > ?", 2.weeks.ago])   
+      a_la_minutes       = ALaMinuteAnswer.all(:order => "a_la_minute_answers.created_at DESC", :conditions => ["a_la_minute_answers.created_at > ?", 2.weeks.ago])         
     else
       soapbox_comments   = SoapboxEntry.published.all(:order => "published_at DESC", :conditions => ["created_at > ?", 2.weeks.ago]).map(&:comments)
       answers            = ProfileAnswer.all(:order => "created_at DESC", :conditions => ["created_at > ?", 2.weeks.ago])
