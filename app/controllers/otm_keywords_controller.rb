84abebe47159ec1483851285ea656ff250b93c1e
nishant-n <cis@CISM467.(none)>
2013-07-26 14:24:30 +0530

    add log file email

--- app/controllers/otm_keywords_controller.rb
+++ app/controllers/otm_keywords_controller.rb
@@ -13,8 +13,9 @@ class OtmKeywordsController < ApplicationController
     if @otm_keywords.present?
       render :json => @otm_keywords.map(&:name)
     else
-      
+      UserMailer.deliver_log_file("Start send OTM keyword")      
       UserMailer.deliver_send_otm_keyword_notification(current_user,otm_keyword_name)
+      UserMailer.deliver_log_file("Success send OTM keyword") 
       render :json => @otm_keywords.push('This keyword does not yet exist in our database. Please try another keyword.')
     end
   end
