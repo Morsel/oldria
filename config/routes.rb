6154227c326162a03c108e795add22b54c4393fe
nishant-n <cis@CISM467.(none)>
2013-08-05 14:51:23 +0530

    remove profile out of date and add request profile add link

--- config/routes.rb
+++ config/routes.rb
@@ -253,7 +253,7 @@ ActionController::Routing::Routes.draw do |map|
 
     restaurant.add_keywords 'add_keywords', :controller => "menu_items", :action => "add_keywords"
 
-    restaurant.profile_out_of_date 'profile_out_of_date', :controller => "restaurants", :action => "profile_out_of_date"
+    restaurant.request_profile_update 'request_profile_update', :controller => "restaurants", :action => "request_profile_update"
 
     restaurant.resources :visitor_emails
 
