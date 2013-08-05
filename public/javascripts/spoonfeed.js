bf72a77f5320a0dc611f9565f633fa82ae6eab7b
Nishant nigam <nishant.n@cisinlabs.com>
2013-08-05 16:11:28 +0530

    Give-ability-for-restaurant-to-upload-an-excel-41007639 done changes

--- public/javascripts/spoonfeed.js
+++ public/javascripts/spoonfeed.js
@@ -811,3 +811,8 @@ function loderHide()
 {
   $('#loader-waiting').slideUp();
 }
+
+var sortedTables = $('.tablesorter');
+if (sortedTables.length) {
+  sortedTables.tablesorter({sortList: [[0,0]]});
+}
