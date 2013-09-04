878e04a2f7cfdd5311dfa665744043e734bb118a
Nishant nigam <nishant.n@cisinlabs.com>
2013-09-04 18:02:13 +0530

    fix error

--- app/models/newsletter_subscriber.rb
+++ app/models/newsletter_subscriber.rb
@@ -24,11 +24,6 @@ class NewsletterSubscriber < ActiveRecord::Base
   has_many :newsletter_subscriptions, :dependent => :destroy
   belongs_to :user
 
-  belongs_to :digest_diner
-  accepts_nested_attributes_for :digest_diner
-  has_many :regional_writers, :dependent => :destroy, :foreign_key => 'newsletter_subscriber_id'
-  has_many :metropolitan_areas_writers, :dependent => :destroy,:foreign_key => 'newsletter_subscriber_id'
-
   validates_presence_of :email
   validates_uniqueness_of :email, :message => "has already registered"
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => "is not a valid email address", :allow_blank => true
@@ -42,7 +37,7 @@ class NewsletterSubscriber < ActiveRecord::Base
 
   before_save :encrypt_password
   after_create :send_confirmation
-  #after_update :update_mailchimp
+  after_update :update_mailchimp
 
   named_scope :confirmed, {
     :conditions => "confirmed_at IS NOT NULL"
@@ -96,88 +91,6 @@ class NewsletterSubscriber < ActiveRecord::Base
   def has_subscription(restaurant)
     NewsletterSubscription.exists?(:newsletter_subscriber_id => self.id, :restaurant_id => restaurant.id)
   end
-
-  # 2 = Regional, 3 = Local
-  def delete_other_writers  
-    if digest_diner_id == 1
-      metropolitan_areas_writers.find(:all,:conditions=>"area_writer_type = 'DigestDiner'").map(&:destroy)      
-      regional_writers.find(:all,:conditions=>"regional_writer_type = 'DigestDiner'").map(&:destroy)
-    elsif  digest_diner_id == 2      
-      metropolitan_areas_writers.find(:all,:conditions=>"area_writer_type = 'DigestDiner'").map(&:destroy)      
-    elsif  digest_diner_id == 3      
-      regional_writers.find(:all,:conditions=>"regional_writer_type = 'DigestDiner'").map(&:destroy)
-    end 
-  end
-
-  def send_newsletters_to_diner
-    for newsletter_subscriber in NewsletterSubscriber.all
-      @arrMedia=[]    
-      @arrMedia.push(newsletter_subscriber.newsletter_subscriptions.map(&:restaurant))
-      @arrMedia.push(newsletter_subscriber.get_digest_subscription)
-      @arrMedia.flatten!
-      unless NewsletterSubscription.menu_items(@arrMedia).blank? && NewsletterSubscription.promotions(@arrMedia).blank?
-        newsletter_subscriber.send_later(:send_newsletter_to_diner_subscribers,newsletter_subscriber) if (!newsletter_subscriber.opt_out) && (!["Sun","Sat"].include?(Date::ABBR_DAYNAMES[Date.today.wday]))
-      end
-    end
-  end
-
-  def get_digest_subscription
-    @restaurants = []    
-    if digest_diner.name == "National Diner"
-      @restaurants = Restaurant.all
-    elsif digest_diner.name == "Regional Diner"
-      @restaurants = digest_diner.find_diner_regional_writers(self).map(&:james_beard_region).map(&:restaurants)
-    else
-      @restaurants = digest_diner.find_diner_metropolitan_areas_writers(self).map(&:metropolitan_area).map(&:restaurants)
-    end unless digest_diner.blank?
-    @restaurants.flatten.compact.uniq
-  end  
-
-  def send_newsletter_to_diner_subscribers subscriber
-    if !subscriber.opt_out 
-      begin
-        @arrMedia=[]    
-        @arrMedia.push(subscriber.newsletter_subscriptions.map(&:restaurant))
-        @arrMedia.push(subscriber.get_digest_subscription)
-        @arrMedia.flatten!
-
-        @basic_restarurants_menu_items = []
-        @basic_restarurants_promotions = []    
-
-        @menu_items = @menus = @restaurantAnswers = @promotions = []
-        unless(@arrMedia.blank?)
-           @arrMedia.each do |restaurant|
-             @basic_restarurants_menu_items << restaurant if !restaurant.premium_account? && !restaurant.menu_items.find(:all,:conditions=>["created_at >= ? OR updated_at >= ?",1.day.ago.beginning_of_day,1.day.ago.beginning_of_day]).blank?
-           end
-           @arrMedia.each do |restaurant|
-             @basic_restarurants_promotions << restaurant if !restaurant.premium_account? && !restaurant.promotions.find(:all,:conditions=>["created_at >= ? OR updated_at >= ?",1.day.ago.beginning_of_day,1.day.ago.beginning_of_day]).blank?
-           end
-
-          @arrMedia = @arrMedia - @basic_restarurants_menu_items - @basic_restarurants_promotions
-
-          @menu_items = NewsletterSubscription.menu_items(@arrMedia)   
-          @menus = NewsletterSubscription.newsletter_menus(@arrMedia)  
-          @fact_sheets = NewsletterSubscription.fact_sheets(@arrMedia)  
-          @photos = NewsletterSubscription.photos(@arrMedia)  
-          @promotions = NewsletterSubscription.promotions(@arrMedia)
-        end
-        diner_user_subscribers = {
-          "subscriber" => subscriber,
-          "basic_restarurants_menu_items" =>@basic_restarurants_menu_items,
-          "basic_restarurants_promotions" => @basic_restarurants_promotions,
-          "menu_items" => @menu_items,
-          "menus" =>@menus,
-          "restaurantAnswers" => @restaurantAnswers,
-          "promotions" => @promotions,
-          "arrMedia" => @arrMedia,
-          "fact_sheets" => @fact_sheets,
-          "photos" => @photos
-        }
-        UserMailer.deliver_send_diner_subscriber(diner_user_subscribers)
-      rescue Exception => e
-        UserMailer.deliver_log_file("User : #{subscriber.email} Error: #{e.message}","Exception")
-      end  
-    end  
 
   def deliver_soapbox_password_reset_instructions!
     reset_perishable_token!
