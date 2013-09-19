0167819de33d8f81b2f2fda631f439ccffc1c94e
Nishant nigam <nishant.n@cisinlabs.com>
2013-09-19 17:56:56 +0530

    undo all the changes of delayed job

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :git_enable_submodules, 1
 set :use_sudo, false
 
 default_run_options[:pty] = true
-require "delayed/recipes" # for delyed job
+
 ##
 # == Staging environment
 #
@@ -172,12 +172,6 @@ namespace :compass do
 
   # Generate all the stylesheets manually (from their Sass templates) before each restart.
 end
-namespace :delayed_job do  
-  desc "Stop delayed_job process" 
-  task :stop, :roles => :app do
-    run "cd #{current_path}; script/delayed_job stop  RAILS_ENV=#{rails_env}" 
-  end
-end
 
 task :logs, :roles => :app do 
   run "cd #{current_path}; RAILS_ENV=#{rails_env} tail -f log/#{rails_env}.log"
@@ -196,7 +190,6 @@ after 'deploy:update_code', 'update_robots'
 
 before "deploy:update_code", "bluepill:stop"
 after "deploy:restart",  "bluepill:start"
-after "deploy:stop", "RAILS_ENV=#{rails_env} delayed_job:stop" 
 
 Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'airbrake-*')].each do |vendored_notifier|
   $: << File.join(vendored_notifier, 'lib')
