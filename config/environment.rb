b50632da62dd4fb061786ac8e96c8169a05694c8
= <=>
2013-09-26 14:26:20 +0530

    remove gem rmagick in gemfile

--- config/environment.rb
+++ config/environment.rb
@@ -44,7 +44,7 @@ Rails::Initializer.run do |config|
   config.gem "ar-octopus",            :version => "0.3.4", :lib => "octopus"
   config.gem "spreadsheet",           :version => "0.8.5"
   config.gem "simple_xlsx_reader" ,   :version => "0.9.7"
-  config.gem "rmagick",             :version => "2.13.2",:git=>'http://github.com/rmagick/rmagick.git' 
+  #config.gem "rmagick",             :version => "2.13.2",:git=>'http://github.com/rmagick/rmagick.git' 
 
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
