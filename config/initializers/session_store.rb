339396add4bbe4f61362a1e5a91b9656a16280ec
Nishant nigam <nishant.n@cisinlabs.com>
2013-11-15 16:25:16 +0530

    fix spoonfeed bug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ Ria::Application.config.session_store :cookie_store, key: '_ria_session'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
- #Ria::Application.config.session_store :mem_cache_store
+Ria::Application.config.session_store :mem_cache_store
