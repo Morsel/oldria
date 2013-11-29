13c0627b84df90a2dbcb7bc4aed2779fea5d395a
Nishant nigam <nishant.n@cisinlabs.com>
2013-11-29 19:40:49 +0530

    fix html issues showin on admin for media issue

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,7 +2,7 @@ DEFAULT_HOST = 'staging.restaurantintelligenceagency.com'
 Ria::Application.config do 
 # Settings specified here will take precedence over those in config/environment.rb
 
-ActionMailer::Base.delivery_method = :sendmail
+#ActionMailer::Base.delivery_method = :sendmail
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
