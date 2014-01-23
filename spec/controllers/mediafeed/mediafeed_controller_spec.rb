5e8cd2c4513aaea7d0af0a3b219f8ed8ceb4c064
Nishant nigam <nishant.n@cisinlabs.com>
2014-01-23 20:06:15 +0530

    rspec test cases for newsleter and othercontroller 23rd jan

--- spec/controllers/mediafeed/mediafeed_controller_spec.rb
+++ spec/controllers/mediafeed/mediafeed_controller_spec.rb
@@ -2,6 +2,15 @@ require_relative '../../spec_helper'
 
 describe Mediafeed::MediafeedController do
 
+  integrate_views
+
+  before(:each) do
+    @user = FactoryGirl.create(:admin)
+    @user.stubs(:update).returns(true)
+    controller.stubs(:current_user).returns(@user)
+    controller.stubs(:require_user).returns(true)
+  end
+
   it "should send a request for information" do
     @sender = FactoryGirl.create(:user)
     controller.stubs(:current_user).returns(@sender)
@@ -12,4 +21,37 @@ describe Mediafeed::MediafeedController do
     DirectMessage.count.should == 1
   end
 
+  describe "GET index" do
+    it "assigns all index as @index" do
+      get :index
+      response.should redirect_to(root_url(:subdomain => "spoonfeed"))
+    end
+  end
+
+  describe "GET login" do
+    it "assigns all index as @login" do
+      get :login
+      response.should redirect_to(login_url(:subdomain => "spoonfeed"))
+    end
+  end
+
+  describe "GET directory" do
+    it "assigns all index as @directory" do
+    	@use_search = true
+    	@users = User.in_soapbox_directory.all(:order => "users.last_name")
+      get :directory
+      assigns[:users].should == @users
+      response.should render_template("layouts/application", "directory/index")
+    end
+  end
+
+  describe "GET directory_search" do
+    it "assigns all index as @directory_search" do
+      get :directory_search
+      response.header['Content-Type'].should include 'text/html'
+    end
+  end
+
+
 end
+
--- spec/controllers/restaurants/behind_the_line_controller_spec.rb
+++ spec/controllers/restaurants/behind_the_line_controller_spec.rb
@@ -1,10 +1,25 @@
 require_relative '../../spec_helper'
 
 describe Restaurants::BehindTheLineController do
-
-  #Delete this example and add some real ones
-  it "should use Restaurants::BehindTheLineController" do
-    controller.should be_an_instance_of(Restaurants::BehindTheLineController)
+integrate_views
+  before(:each) do
+    fake_admin_user
   end
+
+  it "index action should render index template" do
+  	@restaurant = FactoryGirl.create(:restaurant)
+    get :index, restaurant_id: @restaurant.id
+    response.should render_template(:index)
+  end
+
+  it "it should render question_ans_post template" do
+  	@question = FactoryGirl.create(:restaurant_question)
+  	@restaurant = FactoryGirl.create(:restaurant)
+    get :question_ans_post, restaurant_id: @restaurant.id, id: @question.id
+    response.should render_template(:question_ans_post)
+  end
+
+
+
 
 end
