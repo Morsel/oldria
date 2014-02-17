e4894ae55bedf0ee191177b490f371252bfce531
Nishant nigam <nishant.n@cisinlabs.com>
2014-02-17 17:06:55 +0530

    rspec test cases for restaurant_features and other controllers 17th feb

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -25,44 +25,7 @@ describe RestaurantsController do
     end
   end
 
- 
-  # context "GET new" do
-  #   it "should render new template" do
-  #     get :new
-  #     response.should render_template(:new)
-  #   end
-
-  #   it "should assign @restaurant" do
-  #     get :new
-  #     assigns[:restaurant].should be_a(Restaurant)
-  #   end
-
-  #   it "should build restaurant through current_user" do
-  #     get :new
-  #     assigns[:restaurant].manager.should == @user
-  #   end
-  # end
-
-  # context "POST create" do
-  #   it "should build restaurant through current_user" do
-  #     post :create, :restaurant => {}
-  #     assigns[:restaurant].manager.should == @user
-  #   end
-
-  #   it "should render new template when model is invalid" do
-  #     Restaurant.any_instance.stubs(:valid?).returns(false)
-  #     post :create
-  #     response.should render_template(:add_restaurant)
-  #   end
-
-  #   it "should redirect when model is valid" do
-  #     Restaurant.any_instance.stubs(:valid?).returns(true)
-  #     post :create
-  #     response.should redirect_to(bulk_edit_restaurant_employees_path(assigns[:restaurant]))
-  #   end
-  # end
-
-  describe "GET show" do
+   describe "GET show" do
     it "work for show condition" do
       get :show,:id=>@restaurant.id
       response.should render_template(:show)
